cda035fb59168c9c7b96f9a1f10b9564468c1884
illiiiillllilii <vitalik7tv>
2015-11-05 00:56:06 +0300

    Update tables

--- migrations/article_and_comment.sql
+++ migrations/article_and_comment.sql
@@ -1,28 +1,27 @@
 CREATE TABLE `articles` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
   `title` varchar(140) NOT NULL,
+  `description` VARCHAR(140) NOT NULL,
   `text` text NOT NULL,
-  `date` date DEFAULT NULL
+  `cover` varchar(10),
+  `dt_add` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `dt_edit` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 
 CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `article` int(10) DEFAULT NULL,
-  `name` varchar(140) DEFAULT NULL,
-  `comment` text,
-  `answer` int(11) DEFAULT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `article` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `dt_add` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `dt_edit` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `is_removed` TINYINT(1) DEFAULT FALSE NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
 
