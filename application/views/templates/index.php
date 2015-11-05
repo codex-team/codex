4ddb56596473f85fcb8193f3f4cddff861f79f28
Eliseev Alexandr <eliseealex@gmail.com>
2015-11-05 22:54:00 +0300

    Изменено отображения списка айтемов * новое отображение списка айтемов; * изменена навигация; * добавлены новые стили; * изменён шрифт; * изменён порядок айтемов.

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -7,8 +7,8 @@
 <div class="m_logo_wrap">
     <div class="m_logo"></div>
     <a href="/article">Статьи</a>
-      <a href="/join">Подать заявку</a>
-        <a href="/task">Задания</a>
+    <a href="/join">Подать заявку</a>
+    <a href="/task">Задания</a>
 
     <?php if (!$auth->is_guest()): ?>
         <a href='/auth/vk'>Вход VK</a>
