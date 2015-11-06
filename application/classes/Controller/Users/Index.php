16f4bedacf52f683904807a240080904c3948ef1
loopdigga98 <tretyak.1996@gmail.com>
2015-11-06 15:34:59 -0500

    user page

--- application/classes/Controller/Users/Index.php
+++ application/classes/Controller/Users/Index.php
@@ -5,17 +5,17 @@ class Controller_Users_Index extends Controller_Base_preDispatch {
         {
             //load info from DB
             $userId = $this->request->param('user_id');
-            //$userId = 1;
-            if(isset($userId))
-            {
-            	$this->view["users"] = DB::select('*')->from('Users')->where('id', '=', $userId)->execute(); 
-            	$this->view["userId"] = $userId;
-                $this->template->content = View::factory('templates/users/user', $this->view);
+            $user = new Model_User($userId);          
+            
+            if ( $user -> id ){
+            	$viewUser = $user;
+            } else {
+            	$viewUser = $this -> user;
             }
-            else
-            {
-            	$this->view["error"] = "error";
-            	$this->template->content = View::factory('templates/users/user', $this->view);
-            }
+            $this->view["user"] = $viewUser;
+            //$this->view["userId"] = $userId;
+            
+            $this->template->content = View::factory('templates/users/user', $this->view);
+           
         }
     }
