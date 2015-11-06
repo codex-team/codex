3dfb7f15bdd9c9f4d04c545aed80f3c3125541e1
loopdigga98 <tretyak.1996@gmail.com>
2015-11-06 13:58:12 -0500

    user info page

--- application/classes/Controller/Users/Index.php
+++ application/classes/Controller/Users/Index.php
@@ -4,17 +4,18 @@ class Controller_Users_Index extends Controller_Base_preDispatch {
         public function action_showUser()
         {
             //load info from DB
-            //$this->view["users"] = DB::select('*')->from('users')->where('id', '=', $id)->execute(); 
             $userId = $this->request->param('user_id');
-            $this->view = array(
-                    'firstName' => 'John',
-                    'secondName' => 'Smith',
-                    'age' => 25,
-                    'regDate' => '05.11.2015',
-                    'userPhoto' => '/public/img/utilita.jpg',
-                    'info' => 'I am working in big company...',
-                    'userId' => $userId
-            );
-            $this->template->content = View::factory('templates/users/user', $this->view);
+            //$userId = 1;
+            if(isset($userId))
+            {
+            	$this->view["users"] = DB::select('*')->from('Users')->where('id', '=', $userId)->execute(); 
+            	$this->view["userId"] = $userId;
+                $this->template->content = View::factory('templates/users/user', $this->view);
+            }
+            else
+            {
+            	$this->view["error"] = "error";
+            	$this->template->content = View::factory('templates/users/user', $this->view);
+            }
         }
     }
