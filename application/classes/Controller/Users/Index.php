712eb1296dac8315ef0698e65e5f56bf373f94ea
loopdigga98 <tretyak.1996@gmail.com>
2015-11-11 14:35:40 -0500

    pull

--- application/classes/Controller/Users/Index.php
+++ application/classes/Controller/Users/Index.php
@@ -5,19 +5,19 @@ class Controller_Users_Index extends Controller_Base_preDispatch {
         {
             //load info from DB
             $userId = $this->request->param('user_id');
-            $user = new Model_User($userId);          
-            
+            $user = new Model_User($userId);
+
             if ( $user -> id ) {
             	$viewUser = $user;
-            } else {            	
+            } else {
             	$viewUser = $this -> user;
             	$this->view["error"] = "Пожалуйста войдите в аккаунт.";
             }
-            
+
             $this->view["user"] = $viewUser;
             $this->view["userId"] = $userId;
-            
+
             $this->template->content = View::factory('templates/users/user', $this->view);
-           
+
         }
     }
