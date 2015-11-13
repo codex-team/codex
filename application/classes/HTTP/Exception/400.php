5058698f6e5c0eb46dbcebb19674cecca64fed49
Demyashev <alexander.demyashev@gmail.com>
2015-11-14 00:28:26 +0300

    Добавлен обработчик/перехватчик ошибок и шаблон под него

---
+++ application/classes/HTTP/Exception/400.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class HTTP_Exception_400 extends Kohana_HTTP_Exception_400 {
+ 
+    public function get_response()
+    {
+        Kohana_Exception::log($this);
+        
+        $view = View::factory('templates/errors/default');
+        $view->set('title', 'Ошибка в запросе');
+        $view->set('message', $this->getMessage());
+         
+        $response = Response::factory()
+            ->status(400)
+            ->body($view->render());
+ 
+        return $response;
+    }
+}
---
+++ application/classes/HTTP/Exception/401.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class HTTP_Exception_401 extends Kohana_HTTP_Exception_401 {
+ 
+    public function get_response()
+    {   
+        Kohana_Exception::log($this);
+        
+        $response = Response::factory()
+            ->status(401)
+            ->headers('Location', URL::site('/'));
+ 
+        return $response;
+    }
+}
