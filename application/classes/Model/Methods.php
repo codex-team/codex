dd90d02c7aceab5eaf3477e15032c03417744c51
illiiiillllilii <vitalik7tv>
2015-11-20 17:23:59 +0300

    Rebuild comments function has been moved to  Methods

--- application/classes/Model/Methods.php
+++ application/classes/Model/Methods.php
@@ -162,4 +162,36 @@ class Model_Methods extends Model
                 mkdir($dir, $rights);
         }
     }
+
+    function rebuildCommentsTree($comments)
+    {
+        $comments_table_rebuild = array();
+
+        $i = 0;
+        foreach ($comments as $comment):
+            $comments_table_rebuild[] = $comment;
+
+            $var_k = $i;
+            for ($j = 0; $j < $i; $j++) {
+                if ($comment->parent_id == $comments_table_rebuild[$j]->id) {
+                    for ($k = $j + 1; $k < $i; $k++) {
+                        if ($comment->parent_id != $comments_table_rebuild[$k]->parent_id) {
+                            $var_k = $k;
+                            break;
+                        };
+                    };
+                    break;
+                };
+            };
+            for ($j = $i; $j >= $var_k; $j--) {
+                $comments_table_rebuild[$j + 1] = $comments_table_rebuild[$j];
+            }
+
+            $comments_table_rebuild[$var_k] = $comment;
+            $i++;
+        endforeach;
+        array_pop($comments_table_rebuild);
+
+        return $comments_table_rebuild;
+    }
 }
