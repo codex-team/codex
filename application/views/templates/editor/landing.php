9260f807e5f8b4889fab102d5ebec1a8e36ae3a2
neSpecc <specc.dev@gmail.com>
2015-11-22 18:47:27 +0300

    orf. error fix

--- application/views/templates/editor/landing.php
+++ application/views/templates/editor/landing.php
@@ -2,7 +2,7 @@
     <div class="landing_page">
         <h1>CodeX Editor</h1>
         <div class="slogan">
-            We wrote some articles. We now how the best. <br/> Its for free forever.
+            We wrote some articles. We know how the best. <br/> Its for free forever.
         </div>
         <a class="get_button" href="">
             Get for free
