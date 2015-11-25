cd0b4c97f5a672d2b41fa349403ae8c8b521d703
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2015-11-25 21:31:38 +0300

    All in one migration.

--- migrations/25_11_2015_trigger_before_update_comments.sql
+++
@@ -1,4 +0,0 @@
-CREATE TRIGGER before_comments_update BEFORE UPDATE ON Comments 
-FOR EACH ROW
-	SET NEW.dt_update = NOW()
-	
--- migrations/25_11_2015_trigger_before_update_users.sql
+++
@@ -1,4 +0,0 @@
-CREATE TRIGGER before_users_update BEFORE UPDATE ON Users 
-FOR EACH ROW
-	SET NEW.dt_update = NOW()
-	
