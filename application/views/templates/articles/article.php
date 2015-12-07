f45f5f9116ab9a5460b2b03a51626da5c42fb4f5
PolinaShneider <beatrixlinkoln@gmail.com>
2015-12-07 23:02:17 +0300

    Schema Article Markup
    Removed <span>
    Fixed datePublished

--- application/views/templates/articles/article.php
+++ application/views/templates/articles/article.php
@@ -1,98 +1,84 @@
 <div class="center_side clear">
-    <span itemscope itemtype="http://schema.org/Article">
-        <article class="article">
-            <span itemprop="headline">
-            <h1 class="big_header">
-                <?= $article->title ?>
-            </h1>
+    <article class="article" itemscope itemtype="http://schema.org/Article">
+        <h1 class="big_header" itemprop="headline">
+            <?= $article->title ?>
+        </h1>
+        <div class="article_info" itemprop="description">
+            <meta itemprop="datePublished" content=content="date('c',strtotime($article->dt_create))"/><time><?= Date::fuzzy_span($article->dt_create) ?></time>
+            <span class="list_user_ava">
+                <img src="<?= $article->author->photo ?>" alt="<?= $article->author->name ?>" itemprop="image">
             </span>
-                <span itemprop="description">
-                <div class="article_info">
-                <span itemprop="datePublished">
-                <time><?= Date::fuzzy_span($article->dt_create) ?></time>
-                </span>
-                <span class="list_user_ava">
-                    <span itemprop="image">
-                    <img src="<?= $article->author->photo ?>" alt="<?= $article->author->name ?>">
-                    </span>
-                </span>
-                <span itemprop="author">
-                <a class="list_user_name" href="/user/<?= $article->author->id ?>"><?= $article->author->name ?></a>
-                </span>
+            <a class="list_user_name" href="/user/<?= $article->author->id ?>" itemprop="author"><?= $article->author->name ?></a>
+        </div>
+        <div class="article_content" itemprop="articleBody">
+            <?= Text::auto_p($article->text) ?>
+        </div>
+
+
+        <? /*
+
+        <h3>Комментарии</h3>
+        <?
+        if ($comments) {
+            $comment_level = [];
+            foreach ($comments as $comment):
+
+                foreach ($comment_level as $current_comment_level):
+                    if ($current_comment_level > $comment->parent_id) {
+                        array_pop($comment_level);
+                    }
+                endforeach;
+
+                $level = count($comment_level) * 39;
+                $comment_level[] = $comment->id;
+
+
+                if ($comment->user_id == 0) {
+                    $username = 'Гость';
+                } else {
+                    $username = Model_User::get($comment->user_id)->name;
+                };
+                ?>
+
+                <div style='margin: 0 <?= $level ?>px'>
+
+                    <p>
+                        <? if ($comment->user_id == $user->id) {?>
+                            <a href='/article/delcomment/<?= $comment->id ?>'>[удалить]</a>
+                        <? } ?>
+                        <a onclick="document.getElementById('answer_to_comment').value=<?= $comment->id ?>;
+                            document.getElementById('blankCommentTextarea').innerHTML='<?= $username ?>, ';
+                            document.getElementById('answer_username').innerHTML='Ваш ответ на комментарий пользователя <?= $username ?>: ' +
+                            '<i> <?= $comment->text ?></i>';">[ответить]</a>
+
+                        <a href="/user/<?=Model_User::get($comment->user_id)->id ?>"><b> <?= $username ?></b></a>:
+                        <?= $comment->text ?>
+
+                    </p>
+
                 </div>
-                </span>
-            <span itemprop="articleBody">
-            <div class="article_content">
-                <?= Text::auto_p($article->text) ?>
-            </div>
-            </span>
+            <? endforeach; ?>
+        <? } else { ?>
+             <p>Комментариев нет</p>
+        <? } ?>
 
+        <p>
 
-            <? /*
+        <h3 id="answer_username">Выскажи свое мнение</h3>
 
-            <h3>Комментарии</h3>
-            <?
-            if ($comments) {
-                $comment_level = [];
-                foreach ($comments as $comment):
-
-                    foreach ($comment_level as $current_comment_level):
-                        if ($current_comment_level > $comment->parent_id) {
-                            array_pop($comment_level);
-                        }
-                    endforeach;
-
-                    $level = count($comment_level) * 39;
-                    $comment_level[] = $comment->id;
-
-
-                    if ($comment->user_id == 0) {
-                        $username = 'Гость';
-                    } else {
-                        $username = Model_User::get($comment->user_id)->name;
-                    };
-                    ?>
-
-                    <div style='margin: 0 <?= $level ?>px'>
-
-                        <p>
-                            <? if ($comment->user_id == $user->id) {?>
-                                <a href='/article/delcomment/<?= $comment->id ?>'>[удалить]</a>
-                            <? } ?>
-                            <a onclick="document.getElementById('answer_to_comment').value=<?= $comment->id ?>;
-                                document.getElementById('blankCommentTextarea').innerHTML='<?= $username ?>, ';
-                                document.getElementById('answer_username').innerHTML='Ваш ответ на комментарий пользователя <?= $username ?>: ' +
-                                '<i> <?= $comment->text ?></i>';">[ответить]</a>
-
-                            <a href="/user/<?=Model_User::get($comment->user_id)->id ?>"><b> <?= $username ?></b></a>:
-                            <?= $comment->text ?>
-
-                        </p>
-
-                    </div>
-                <? endforeach; ?>
-            <? } else { ?>
-                 <p>Комментариев нет</p>
-            <? } ?>
+        <form method="POST" action="/article/addcomment">
+            <input type="hidden" name="article_id" value="<?= $article->id ?>"/>
+            <input type="hidden" name="parent_id" value="0" id="answer_to_comment"/>
+            <label for="blankCommentTextarea">Комментарий</label>
+            <textarea name="text" id="blankCommentTextarea" required></textarea>
 
             <p>
-
-            <h3 id="answer_username">Выскажи свое мнение</h3>
-
-            <form method="POST" action="/article/addcomment">
-                <input type="hidden" name="article_id" value="<?= $article->id ?>"/>
-                <input type="hidden" name="parent_id" value="0" id="answer_to_comment"/>
-                <label for="blankCommentTextarea">Комментарий</label>
-                <textarea name="text" id="blankCommentTextarea" required></textarea>
-
-                <p>
-                    <button class="master" id="blankSendButton">Добавить комментарий</button>
-                </p>
-            </form>
+                <button class="master" id="blankSendButton">Добавить комментарий</button>
             </p>
+        </form>
+        </p>
 
-                    *******/ ?>
+                *******/ ?>
 
-        </article>
-    </span>
+    </article>
 </div>
