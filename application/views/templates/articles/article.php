3290f2acb4bbcead0fc71d88ca81c7133cf4663b
PolinaShneider <beatrixlinkoln@gmail.com>
2015-12-10 18:41:22 +0300

    Revert "Schemaarticle"

--- application/views/templates/articles/article.php
+++ application/views/templates/articles/article.php
@@ -1,22 +1,20 @@
 <div class="center_side clear">
     <article class="article" itemscope itemtype="http://schema.org/Article">
-        <h1 class="big_header" itemprop="headline">
+
+        <h1 class="big_header" itemprop="name">
             <?= $article->title ?>
         </h1>
+
         <div class="article_info">
-        <div class="ava_holder">
-            <div itemscope itemtype="http://schema.org/Person" itemprop="author">
-                <meta itemprop="datePublished" content="<?= date(DATE_ISO8601, strtotime($article->dt_create)) ?>" />
+            <div class="ava_holder">
                 <time itemprop="datePublished"><?= Date::fuzzy_span($article->dt_create) ?></time>
                 <span class="list_user_ava">
-                    <img src="<?= $article->author->photo ?>" alt="<?= $article->author->name ?>"  itemprop="image">
+                    <img src="<?= $article->author->photo ?>" alt="<?= $article->author->name ?>">
                 </span>
-                <meta itemprop="name" content="<?= $article->author->name ?>" />
-                <meta itemprop="memberOf" content="CodeX" />
-                <a class="list_user_name" itemprop="url" href="/user/<?= $article->author->id ?>"><?= $article->author->name ?></a>
+                <a class="list_user_name" href="/user/<?= $article->author->id ?>" itemscope itmetype="http://schema.org/Person"><?= $article->author->name ?></a>
             </div>
         </div>
-        </div>
+
         <div class="article_content"  itemprop="articleBody">
             <?= nl2br($article->text) ?>
         </div>
