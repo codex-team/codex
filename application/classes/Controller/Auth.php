d85c8f4202a6d69938a29520dbd79c0bb61f9506
Alexander Menshikov <ntpcp@yandex.ru>
2015-12-24 19:06:42 +0700

    Copy redirect to actions

--- application/classes/Controller/Auth.php
+++ application/classes/Controller/Auth.php
@@ -44,7 +44,7 @@ class Controller_Auth extends Controller_Base_preDispatch
         {
             # Add auth error view
         }
-        $this->auth_callback();
+        Controller::redirect($this->get_return_url());
 
     }
 
@@ -86,7 +86,7 @@ class Controller_Auth extends Controller_Base_preDispatch
         {
 
         }
-        $this->auth_callback();
+        Controller::redirect($this->get_return_url());
     }
 
 
@@ -143,7 +143,7 @@ class Controller_Auth extends Controller_Base_preDispatch
         {
 
         }
-        $this->auth_callback();
+        Controller::redirect($this->get_return_url());
     }
 
 
@@ -153,25 +153,16 @@ class Controller_Auth extends Controller_Base_preDispatch
     public function action_logout()
     {
         Cookie::delete("auth_token");
-        Controller::redirect('/');
+        Controller::redirect($this->get_return_url());
     }
 
 
     /**
-     * Место для пост-авторизации. В конце осуществляет редирект страницу $page.
-     * @param $page - страница для редиректа. Если не задана - редирект идет по history_back.
+     * @return URL для возвращения на предыдущую страницу
      */
-    private function auth_callback($page='')
+    private function get_return_url()
     {
-        if ($page)
-        {
-            Controller::redirect($page);
-        }
-        else
-        {
-            $ref = Request::initial()->referrer();
-            Controller::redirect($ref);
-        }
+        return Request::initial()->referrer();
     }
 
 
