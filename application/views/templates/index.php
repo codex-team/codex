250be49eaf4e4cc9215920f3bfaeaf0f1cf71803
neSpecc <specc.dev@gmail.com>
2016-01-29 18:44:13 +0300

    remove new year landing

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -1,9 +1,15 @@
 <?
     /** New Year Landing */
-    include 'landings/new_year.php';
+    // include 'landings/new_year.php';
 ?>
 <div class="clarity_page">
     <div class="center_side">
+
+        <div class="contest_alert">
+            <a href="/contest/2">конкурс на создание ui kit »</a>
+            <div class="line"></div>
+        </div>
+
         <div class="cdx_logo"></div>
         <p>CodeX — это новый клуб в НИУ ИТМО, объединяющий студентов и выпускников, интересующихся веб-разработкой, дизайном и изучением новых технологий на практике.</p>
         <p>Наша цель — собрать команду молодых специалистов с горящими глазами и идеалистическим настроем.</p>
