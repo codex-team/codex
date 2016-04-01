614e3096be1b73c0dff95fa25015c651dbd3ec94
Murod <murod.haydarov@gmail.com>
2016-04-01 20:45:27 +0300

    fix

--- application/classes/Model/Alias.php
+++ application/classes/Model/Alias.php
@@ -199,49 +199,17 @@ class Model_Alias
 
     public static function generateUri( $string )
     {
-        $converter = array(
-                'а' => 'a',   'б' => 'b',   'в' => 'v',
-                'г' => 'g',   'д' => 'd',   'е' => 'e',
-                'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
-                'и' => 'i',   'й' => 'y',   'к' => 'k',
-                'л' => 'l',   'м' => 'm',   'н' => 'n',
-                'о' => 'o',   'п' => 'p',   'р' => 'r',
-                'с' => 's',   'т' => 't',   'у' => 'u',
-                'ф' => 'f',   'х' => 'h',   'ц' => 'c',
-                'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
-                'ь' => "",    'ы' => 'y',   'ъ' => "",
-                'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+        $model_methods = new Model_Methods();
+        $converted_string = $model_methods->rus2translit( $string );
 
-                'А' => 'A',   'Б' => 'B',   'В' => 'V',
-                'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
-                'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
-                'И' => 'I',   'Й' => 'Y',   'К' => 'K',
-                'Л' => 'L',   'М' => 'M',   'Н' => 'N',
-                'О' => 'O',   'П' => 'P',   'Р' => 'R',
-                'С' => 'S',   'Т' => 'T',   'У' => 'U',
-                'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
-                'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
-                'Ь' => "",    'Ы' => 'Y',   'Ъ' => "",
-                'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
-                ' ' => '-',   '-' => '-',   '-' => '-',    '.' => '-',
-                ',' => '-',   '\'' => '',   '\"' => '',    '(' => '-', ')' => '-',
-                '?' => '-',   '#' => '-',   '$' => '-',    '!' => '-',
-                '@' => '-',   '%' => '-',   '&' => '-',    '*' => '-',
-                '`' => '-',   '\\' => '-',  '/' => '-'//,    '*' => '-'
-            );
-            // translit
-            $tmp = strtr($string, $converter);
-            // remove underline from begin and end of line
-            $tmp = trim($tmp, "_");
-            // replace lines
-            $tmp = strtr($tmp, array(
-                "__"    => "_",
-                "___"   => "_",
-                "____"  => "_",
-                "_____" => "_",
-            ));
+        $converted_string = preg_replace("/[^0-9a-zA-Z]/", "-", $converted_string);
+        // заменяем несколько дефисов на один
+        $converted_string = preg_replace('/-{2,}/', '-', $converted_string);
+        // отсекаем лишние дефисы по краям
+        $converted_string = trim($converted_string, '-');
 
-            return strtolower( $tmp );
+
+        return $converted_string;
     }
 
 }
