3ca16cdf6608870a2745b35d139afb28ab4f0602
khaydarov <murod.haydarov@inbox.ru>
2016-07-05 01:53:18 +0300

    init added

--- modules/aliases/init.php
+++ modules/aliases/init.php
@@ -5,22 +5,23 @@
  * @author CodeX team team@ifmo.su
  */
 
-
-$DIGIT = '\d+';
 $STRING = '[-a-z\d]+';
-$QUERY =  '[0-9a-zA-Zа-яёА-ЯЁ\s\-\.]+$';
 
 Route::prepend('URI', '<route>(/<subaction>)', array(
-    'route' => $STRING,
-))->filter(function(Route $route, $params, Request $request)
-{
-    $alias = $params['route'];
-    $model_uri = Model_Uri::Instance();
-    if ( $model_uri->isForbidden($alias) ) {
-        return false;
-    }
 
-})
+    'route' => $STRING,
+
+))
+    ->filter( function(Route $route, $params, Request $request ) {
+
+        $alias = $params['route'];
+        $model_uri = Model_Uri::Instance();
+
+        if ( $model_uri->isForbidden($alias) ) {
+            return false;
+        }
+
+    })
     ->defaults(array(
         'controller' => 'Uri',
         'action' => 'get',
