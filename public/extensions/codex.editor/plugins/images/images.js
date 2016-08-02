e3cf54e05953ddd8eb2e30eb0c152f235ba87be5
khaydarov <murod.haydarov@inbox.ru>
2016-08-02 17:58:25 +0300

    tabulation with 4 spaces

--- public/extensions/codex.editor/plugins/images/images.js
+++ public/extensions/codex.editor/plugins/images/images.js
@@ -6,209 +6,206 @@
 */
 var ceImage = {
 
-  /** Default path to redactors images */
-  path : '/upload/redactor_images/',
+    /** Default path to redactors images */
+    path : '/upload/redactor_images/',
 
-  make : function ( data ) {
+    make : function ( data ) {
 
-    /**
-    * If we can't find image or we've got some problems with image path, we show plugin uploader
-    */
-    if (!data || !data.file.url) {
-      holder = ceImage.ui.formView();
-    } else {
-      holder = ceImage.ui.imageView(data);
-    }
+        /**
+        * If we can't find image or we've got some problems with image path, we show plugin uploader
+        */
+        if (!data || !data.file.url) {
+            holder = ceImage.ui.formView();
+        } else {
+            holder = ceImage.ui.imageView(data);
+        }
 
-    return holder;
+        return holder;
+    },
 
-  },
+    render : function( data ) {
 
-  render : function( data ) {
-
-	  return this.make(data);
+        return this.make(data);
 
 	},
 
-  save : function ( block ) {
+    save : function ( block ) {
 
-    var data = block[0],
-        image = data.querySelector('.ce-plugin-image__uploaded'),
-        caption = data.querySelector('.ce-plugin-image--caption');
+        var data = block[0],
+            image = data.querySelector('.ce-plugin-image__uploaded'),
+            caption = data.querySelector('.ce-plugin-image--caption');
 
-    var json = {
-        type : 'image',
-        data : {
-          background : false,
-          border : false,
-          isStrech : false,
-          file : {
-            url : image.src,
-            bigUrl : null,
-            width  : image.width,
-            height : image.height,
-            additionalData :null,
-          },
-        caption : caption.textContent,
-        cover : null,
-        }
-      }
+        var json = {
+            type : 'image',
+            data : {
+                background : false,
+                border : false,
+                isStrech : false,
+                file : {
+                    url : image.src,
+                    bigUrl : null,
+                    width  : image.width,
+                    height : image.height,
+                    additionalData :null,
+                },
+                caption : caption.textContent,
+                cover : null,
+            }
+        };
 
-    return json;
+        return json;
 
 	},
 
 	uploadButtonClicked : function(event){
 
-    var success = ceImage.callbacks.success,
-        error   = ceImage.callbacks.error;
+        var success = ceImage.callbacks.success,
+            error   = ceImage.callbacks.error;
 
-    /** Define callbacks */
-		cEditor.transport.selectAndUpload({
-      success,
-      error,
-    });
-
-	}
+        /** Define callbacks */
+        cEditor.transport.selectAndUpload({
+            success,
+            error,
+        });
+    }
 };
 
 ceImage.ui = {
 
-  holder : function(){
+    holder : function(){
 
-    var element = document.createElement('DIV');
+        var element = document.createElement('DIV');
 
-    element.classList.add('ce-plugin-image__holder');
+        element.classList.add('ce-plugin-image__holder');
 
-    return element;
+        return element;
+    },
 
-  },
+    input : function(){
 
-  input : function(){
+        var input = document.createElement('INPUT');
 
-    var input = document.createElement('INPUT');
+        return input;
 
-    return input;
+    },
 
-  },
+    uploadButton : function(){
 
-  uploadButton : function(){
+        var button = document.createElement('SPAN');
 
-    var button = document.createElement('SPAN');
+        button.classList.add('ce-plugin-image__button');
 
-    button.classList.add('ce-plugin-image__button');
+        button.innerHTML = '<i class="ce-icon-picture"></i>';
 
-    button.innerHTML = '<i class="ce-icon-picture"></i>';
+        return button;
 
-    return button;
+    },
 
-  },
+    /**
+    * @param {string} source - file path
+    * @param {string} style - css class
+    * @return {object} image - document IMG tag
+    */
+    image : function(source, style) {
 
-  /**
-  * @param {string} source - file path
-  * @param {string} style - css class
-  * @return {object} image - document IMG tag
-  */
-  image : function(source, style) {
+        var image = document.createElement('IMG');
 
-    var image = document.createElement('IMG');
+        image.classList.add(style);
 
-    image.classList.add(style);
+        image.src = source;
 
-    image.src = source;
+        return image;
+    },
 
-    return image;
-  },
+    wrapper : function() {
 
-  wrapper : function() {
+        var div = document.createElement('div');
 
-    var div = document.createElement('div');
+        div.classList.add('ce-plugin-image__wrapper');
 
-    div.classList.add('ce-plugin-image__wrapper');
+        return div;
+    },
 
-    return div;
-  },
+    caption : function() {
 
-  caption : function() {
+        var div = document.createElement('div');
 
-    var div = document.createElement('div');
+        div.classList.add('ce-plugin-image--caption');
 
-    div.classList.add('ce-plugin-image--caption');
+        div.contentEditable = true;
 
-    div.contentEditable = true;
+        return div;
+    },
 
-    return div;
-  },
+    /**
+    * Draws form for image upload
+    */
+    formView : function() {
 
-  /**
-  * Draws form for image upload
-  */
-  formView : function() {
+        var holder       = ceImage.ui.holder(),
+            uploadButton = ceImage.ui.uploadButton(),
+            input        = ceImage.ui.input();
 
-    var holder       = ceImage.ui.holder(),
-        uploadButton = ceImage.ui.uploadButton(),
-        input        = ceImage.ui.input();
+        input.placeholder = 'Paste image URL or file';
 
-    input.placeholder = 'Paste image URL or file';
+        holder.appendChild(uploadButton);
+        holder.appendChild(input);
 
-    holder.appendChild(uploadButton);
-    holder.appendChild(input);
+        uploadButton.addEventListener('click', ceImage.uploadButtonClicked, false );
 
-    uploadButton.addEventListener('click', ceImage.uploadButtonClicked, false );
+        return holder;
+    },
 
-    return holder;
-  },
+    /**
+    * wraps image and caption
+    * @param {object} data - image information
+    * @return wrapped block with image and caption
+    */
+    imageView : function(data) {
 
-  /**
-  * wraps image and caption
-  * @param {object} data - image information
-  * @return wrapped block with image and caption
-  */
-  imageView : function(data) {
+        var file = data.file.url,
+            text = data.caption,
+            type     = data.type,
+            image    = ceImage.ui.image(file, 'ce-plugin-image__uploaded'),
+            caption  = ceImage.ui.caption(),
+            wrapper  = ceImage.ui.wrapper();
 
-    var file = data.file.url,
-        text = data.caption,
-        type     = data.type,
-        image    = ceImage.ui.image(file, 'ce-plugin-image__uploaded'),
-        caption  = ceImage.ui.caption(),
-        wrapper  = ceImage.ui.wrapper();
+        caption.textContent = text;
 
-    caption.textContent = text;
+        /** Appeding to the wrapper */
+        wrapper.appendChild(image);
+        wrapper.appendChild(caption);
 
-    /** Appeding to the wrapper */
-    wrapper.appendChild(image);
-    wrapper.appendChild(caption);
-
-    return wrapper;
-  }
+        return wrapper;
+    }
 
 };
 
 ceImage.callbacks = {
 
-  success : function(result) {
+    success : function(result) {
 
-    var parsed   = JSON.parse(result),
-        filename = parsed.filename,
-        image    = ceImage.ui.image(ceImage.path + 'o_' + filename, 'ce-plugin-image__uploaded'),
-        caption  = ceImage.ui.caption(),
-        img_wrapper = ceImage.ui.wrapper();
+        var parsed   = JSON.parse(result),
+            filename = parsed.filename,
+            image    = ceImage.ui.image(ceImage.path + 'o_' + filename, 'ce-plugin-image__uploaded'),
+            caption  = ceImage.ui.caption(),
+            img_wrapper = ceImage.ui.wrapper();
 
-    img_wrapper.appendChild(image);
-    img_wrapper.appendChild(caption);
+        img_wrapper.appendChild(image);
+        img_wrapper.appendChild(caption);
 
-    /** Replace plugin form with image */
-    var wrapper = cEditor.content.composeNewBlock(img_wrapper, 'image'),
-        nodeToReplace = cEditor.content.currentNode;
+        /** Replace plugin form with image */
+        var wrapper = cEditor.content.composeNewBlock(img_wrapper, 'image'),
+            nodeToReplace = cEditor.content.currentNode;
 
-    cEditor.content.replaceBlock(nodeToReplace, wrapper, 'image');
+        cEditor.content.replaceBlock(nodeToReplace, wrapper, 'image');
 
-  },
+    },
 
-  error : function(result) {
-    console.log('Choosen file is not image or image is corrupted');
-    cEditor.notifications.errorThrown();
-  },
+    error : function(result) {
+        console.log('Choosen file is not image or image is corrupted');
+        cEditor.notifications.errorThrown();
+    },
 
 }
 
@@ -218,10 +215,10 @@ ceImage.callbacks = {
 */
 cEditor.tools.image = {
 
-  type           : 'image',
-  iconClassname  : 'ce-icon-picture',
-  make           : ceImage.make,
-  render         : ceImage.render,
-  save           : ceImage.save
+    type           : 'image',
+    iconClassname  : 'ce-icon-picture',
+    make           : ceImage.make,
+    render         : ceImage.render,
+    save           : ceImage.save
 
 };
