8f9f5adaf40ecc4bff75c28da97f096a487a5a0d
khaydarov <murod.haydarov@inbox.ru>
2016-08-02 16:55:56 +0300

    updated variables name

--- public/extensions/codex.editor/plugins/images/images.js
+++ public/extensions/codex.editor/plugins/images/images.js
@@ -15,9 +15,9 @@ var ceImage = {
         * If we can't find image or we've got some problems with image path, we show plugin uploader
         */
         if (!data || !data.file.url) {
-            holder = ceImage.ui.form();
+            holder = ceImage.ui.formView();
         } else {
-            holder = ceImage.ui.image(data);
+            holder = ceImage.ui.imageView(data);
         }
 
 		return holder;
@@ -64,9 +64,9 @@ var ceImage = {
 
             var parsed   = JSON.parse(result),
                 filename = parsed.filename,
-                image    = ceImage.ui.imageHolder(ceImage.path + 'o_' + filename, 'ce-plugin-image__uploaded'),
+                image    = ceImage.ui.image(ceImage.path + 'o_' + filename, 'ce-plugin-image__uploaded'),
                 caption  = ceImage.ui.caption(),
-                img_wrapper = ceImage.ui.imageWrapper();
+                img_wrapper = ceImage.ui.wrapper();
 
             img_wrapper.appendChild(image);
             img_wrapper.appendChild(caption);
@@ -90,117 +90,120 @@ var ceImage = {
             error,
         });
 
-	},
-
-	ui : {
-
-		holder : function(){
-
-			var element = document.createElement('DIV');
-
-			element.classList.add('ce-plugin-image__holder');
-
-			return element;
-
-		},
-
-		input : function(){
-
-			var input = document.createElement('INPUT');
-
-			return input;
-
-		},
-
-		uploadButton : function(){
-
-			var button = document.createElement('SPAN');
-
-			button.classList.add('ce-plugin-image__button');
-
-			button.innerHTML = '<i class="ce-icon-picture"></i>';
-
-			return button;
-
-		},
-
-        /**
-        * @param {string} source - file path
-        * @param {string} style - css class
-        */
-        imageHolder : function(source, style) {
-
-            var image = document.createElement('IMG');
-
-            image.classList.add(style);
-
-            image.src = source;
-
-            return image;
-        },
-
-        /**
-        * Wrapper for image
-        */
-        imageWrapper : function() {
-
-            var div = document.createElement('div');
-
-            div.classList.add('ce-plugin-image__wrapper');
-
-            return div;
-        },
-
-        caption : function() {
-
-            var div = document.createElement('div');
-
-            div.classList.add('ce-plugin-image--caption');
-            div.contentEditable = true;
-
-            return div;
-        },
-
-        /**
-        * Draws form for image upload
-        */
-        form : function() {
-
-            var holder       = ceImage.ui.holder(),
-                uploadButton = ceImage.ui.uploadButton(),
-                input        = ceImage.ui.input();
-
-            input.placeholder = 'Paste image URL or file';
-
-            holder.appendChild(uploadButton);
-            holder.appendChild(input);
-
-            uploadButton.addEventListener('click', ceImage.uploadButtonClicked, false );
-
-            return holder;
-
-        },
-
-        image : function(data) {
-
-            var file = data.file.url,
-                text = data.caption,
-                type     = data.type,
-                image    = ceImage.ui.imageHolder(file, 'ce-plugin-image__uploaded'),
-                caption  = ceImage.ui.caption(),
-                wrapper  = ceImage.ui.imageWrapper();
-
-            caption.textContent = text;
-
-            /** Appeding to the wrapper */
-            wrapper.appendChild(image);
-            wrapper.appendChild(caption);
-
-            return wrapper;
-        }
 	}
-
 };
+
+ceImage.ui = {
+
+    holder : function(){
+
+        var element = document.createElement('DIV');
+
+        element.classList.add('ce-plugin-image__holder');
+
+        return element;
+
+    },
+
+    input : function(){
+
+        var input = document.createElement('INPUT');
+
+        return input;
+
+    },
+
+    uploadButton : function(){
+
+        var button = document.createElement('SPAN');
+
+        button.classList.add('ce-plugin-image__button');
+
+        button.innerHTML = '<i class="ce-icon-picture"></i>';
+
+        return button;
+
+    },
+
+    /**
+    * @param {string} source - file path
+    * @param {string} style - css class
+    * @return {object} image - document IMG tag
+    */
+    image : function(source, style) {
+
+        var image = document.createElement('IMG');
+
+        image.classList.add(style);
+
+        image.src = source;
+
+        return image;
+    },
+
+    wrapper : function() {
+
+        var div = document.createElement('div');
+
+        div.classList.add('ce-plugin-image__wrapper');
+
+        return div;
+    },
+
+    caption : function() {
+
+        var div = document.createElement('div');
+
+        div.classList.add('ce-plugin-image--caption');
+        div.contentEditable = true;
+
+        return div;
+    },
+
+    /**
+    * Draws form for image upload
+    */
+    formView : function() {
+
+        var holder       = ceImage.ui.holder(),
+            uploadButton = ceImage.ui.uploadButton(),
+            input        = ceImage.ui.input();
+
+        input.placeholder = 'Paste image URL or file';
+
+        holder.appendChild(uploadButton);
+        holder.appendChild(input);
+
+        uploadButton.addEventListener('click', ceImage.uploadButtonClicked, false );
+
+        return holder;
+
+    },
+
+    /**
+    * appends into div image and caption
+    * @param {object} data - image information
+    * @return wrapped block with image and caption
+    */
+    imageView : function(data) {
+
+        var file = data.file.url,
+            text = data.caption,
+            type     = data.type,
+            image    = ceImage.ui.image(file, 'ce-plugin-image__uploaded'),
+            caption  = ceImage.ui.caption(),
+            wrapper  = ceImage.ui.wrapper();
+
+        caption.textContent = text;
+
+        /** Appeding to the wrapper */
+        wrapper.appendChild(image);
+        wrapper.appendChild(caption);
+
+        return wrapper;
+    }
+};
+
 
 /**
 * Add plugin it to redactor tools
