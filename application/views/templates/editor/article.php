4a508d7f90788484ef5c13a485657693f526b65a
khaydarov <murod.haydarov@inbox.ru>
2016-09-27 12:22:18 +0300

    fixed

--- application/views/templates/editor/article.php
+++ application/views/templates/editor/article.php
@@ -4,13 +4,7 @@
         <h1 class="big_header" itemprop="headline">
             <?= $article->title; ?>
         </h1>
-        <div class="article_info">
-            <div class="ava_holder" itemscope itemtype="http://schema.org/Person" itemprop="author">
-
-                <meta itemprop="url" href="https://ifmo.su/user/<?= $article->user_id; ?>" />
-
-            </div>
-        </div>
+        
         <div class="article_content"  itemprop="articleBody">
 
             <? for($i = 0; $i < count($render); $i++) : ?>
