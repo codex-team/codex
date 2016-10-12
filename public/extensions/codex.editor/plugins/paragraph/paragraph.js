419ae60dc9b689bdc4f5dcf25f4d2830805f2718
khaydarov <murod.haydarov@inbox.ru>
2016-10-12 19:37:18 +0300

    upd

--- public/extensions/codex.editor/plugins/paragraph/paragraph.js
+++ public/extensions/codex.editor/plugins/paragraph/paragraph.js
@@ -20,7 +20,11 @@ var paragraphTool = {
         }
 
         tag.contentEditable = true;
-        tag.setAttribute('data-placeholder', '');
+
+        /**
+         * if plugin need to add placeholder
+         * tag.setAttribute('data-placeholder', 'placehoder');
+         */
 
         return tag;
 
