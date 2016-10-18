8187dbdb34ae288cd69d0a60c2b8ffe17738a8a8
Peter Savchenko <specc.dev@gmail.com>
2016-10-18 16:20:56 +0300

    improve meet up landign

--- application/views/templates/landings/meetup.php
+++ application/views/templates/landings/meetup.php
@@ -3,6 +3,6 @@
         <div class="event_alert__day">21</div>
         <div class="event_alert__month">октября</div>
         <div class="event_alert__title">вступительное собрание</div>
-        <div class="event_alert__description branchy">&nbsp;18:00, главный корпус&nbsp;</div>
+        <div class="event_alert__description branchy">&nbsp;главный корпус, ауд. 403 <i class="event_alert__icon_delimiter icon-users"></i> 18:00&nbsp;</div>
     </a>
 </div>
