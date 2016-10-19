3db2fcea4c28ffd2f16e01a279727d067f3e7f8d
Taly <guryn@yandex.ru>
2016-10-20 01:28:27 +0300

    action for Special landing

--- application/classes/Controller/Landings.php
+++ application/classes/Controller/Landings.php
@@ -4,11 +4,19 @@ class Controller_Landings extends Controller_Base_preDispatch
 {
 
     /** Codex Bot Landing page  in https://ifmo.su/ */
-    public function action_show()
+    public function action_bot()
     {
         $this->title = 'CodeX Bot';
         $this->description = 'Description for CodeX Bot';
         $this->template->content = View::factory('templates/bot/landing', $this->view);
+    }
+
+    /** Codex Special Landing page in https://ifmo.su/special */
+    public function action_special()
+    {
+        $this->title = 'CodeX Special';
+        $this->description = 'Module for making high contrast site version';
+        $this->template->content = View::factory('templates/special/landing', $this->view);
     }
 
 }
