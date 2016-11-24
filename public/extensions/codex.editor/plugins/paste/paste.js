309cbd1279ce6201468be45d9b328b791eefba8b
khaydarov <murod.haydarov@inbox.ru>
2016-11-24 21:00:27 +0300

    upd

--- public/extensions/codex.editor/plugins/paste/paste.js
+++ public/extensions/codex.editor/plugins/paste/paste.js
@@ -86,6 +86,7 @@ var pasteTool = {
              */
             json = {
                 type: "instagram",
+                tool: "paste",
                 data: {
                     media: true,
                     user: {
@@ -105,6 +106,7 @@ var pasteTool = {
              */
             json = {
                 type: "tweet",
+                tool: "paste",
                 data: {
                     media: true,
                     conversation: false,
