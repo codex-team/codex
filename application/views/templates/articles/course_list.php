3b7bb049be628e07d1fa01b8a1f326b339391292
khaydarov <murod.haydarov@inbox.ru>
2016-12-12 01:31:53 +0300

    improved one more time

--- application/views/templates/articles/course_list.php
+++ application/views/templates/articles/course_list.php
@@ -2,12 +2,13 @@
 
     <h2 class="course__title"><?=$course->title; ?></h2>
 
-    <ul class="courses-list">
-        <? for($i = 0; isset($articles) && $i < count($articles); $i++) : ?>
-            <li class="courses-list__item">
-                <a href="<?=URL::site($articles[$i]->uri ?: '/article/ ' . $articles[$i]->id ); ?>" class="courses-list__link" href=""><?=$articles[$i]->title; ?></a>
-            </li>
-        <? endfor; ?>
-    </ul>
-
+    <? if (!empty($articles)) : ?>
+        <ul class="courses-list">
+            <? foreach ($articles as $article) : ?>
+                <li class="courses-list__item">
+                    <a href="<?=URL::site($article->uri ?: '/article/ ' . $article->id ); ?>" class="courses-list__link" href=""><?=$article->title; ?></a>
+                </li>
+            <? endforeach; ?>
+        </ul>
+    <? endif; ?>
 </section>
