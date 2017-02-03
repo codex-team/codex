4fb09937b551b26b89b9bd6157d00f253336f95c
Peter Savchenko <specc.dev@gmail.com>
2017-02-03 20:40:52 +0300

    Implement feedback module

---
+++ application/classes/Controller/Games.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Games extends Controller_Base_preDispatch
+{
+ adffdwadgfw
+    public function action_show ()
+    {
+        
+        
+
+    }
+
+}
