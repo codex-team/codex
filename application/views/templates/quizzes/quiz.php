500a710179fe857ed30b9e4b787006b2bf4879ab
Polina Shneider <beatrixlinkoln@gmail.com>
2017-06-09 21:14:25 +0300

    Closes #347 task 25

--- application/views/templates/quizzes/quiz.php
+++ application/views/templates/quizzes/quiz.php
@@ -1,8 +1,5 @@
-<link rel="stylesheet" href="/public/css/quiz.css?v=<?= filemtime('public/css/quiz.css') ?>">
-<script src="/public/js/quiz.js?v=<?= filemtime('public/js/quiz.js') ?>"></script>
-
 <div id="quiz"></div>
 
 <script>
-    quiz.init( <?= $quizData; ?>, 'quiz');
+    codex.quiz.init( <?= $quizData; ?>, 'quiz');
 </script>
