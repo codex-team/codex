91b7492ab2c6105c302e252d6467be7b0a669cad
Polina Shneider <beatrixlinkoln@gmail.com>
2017-07-04 23:01:48 +0300

    BEM naming improvement thanks to schema drawing
    Moved new bot landing to separate file, left old bot landing for
    possible further uses

---
+++ application/views/templates/landings/codex_bot.php
@@ -0,0 +1,75 @@
+<link rel="stylesheet" href="/public/css/landings/bot.css?v=<?= filemtime("public/css/landings/codex_bot.css") ?>">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+
+<div class="center_side">
+    <div class="landing">
+        <div class="landing__about about">
+            <div class="description">
+                <img class="description__img" src="/public/img/landings/bot/codex_bot_logo.png"> 
+                <div class="description-content">
+                    <h2 class="description__info">Working team assistant</h2>
+                    <div class="description__name">@codex_bot</div>
+                    <div class="description__links">
+                        <a class="description__links-item description__links-item--first" href="">Getting Started </a>
+                        <a class="description__links-item" href="">Documentation</a>
+                        <a class="description__links-item" href="">View code</a>
+                    </div>
+                </div>
+            </div>
+            <div class="about__buttons">
+                <a class="button" href="">
+                    <img class="button__img" src="/public/img/landings/bot/slack_cmyk.png">
+                    Add to <br><b>Slack</b>
+                </a>
+                <a class="button" href="">
+                    <img class="button__img" src="/public/img/landings/bot/TelegramLogo2.png">
+                    Add to <br><b>Telegram</b>
+                </a>
+            </div>
+        </div>
+        <div class="integrations">
+            <div class="integrations__title-wrapper">
+                <h2 class="integrations__title">Available integrations</h2>
+                <a class="integrations__link" href="">How to create your own</a>
+            </div>
+            <div class="integrations__items-wrapper">
+                <div class="integrations__item integrations__item--first">
+                    <img class="integrations__item-logo" src="/public/img/landings/bot/github.png">
+                    <div class="integrations__item-content">
+                        <div class="integrations__item-title"><b>GitHub</b></div>
+                        <div class="integrations__item-desc">
+                            Notifications about new issues, pull-requests, reviews. And other helpfully stuff.
+                        </div>
+                        <div class="integrations__item-footer">
+                            <div class="integrations__item-contributers">
+                                <div class="integrations__item-dev"></div>
+                                <div class="integrations__item-dev"></div>
+                                <div class="integrations__item-dev"></div>
+                                <div class="integrations__item-dev"></div>
+                            </div>
+                            <a class="integrations__item-link" href="">github.com/codex-bot/github</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="integrations__item">
+                    <div class="integrations__item-content">
+                        <div class="integrations__item-title">
+                            <img class="integrations__item-yandex" src="/public/img/landings/bot/yandex_eng_logo@1,5x.png">
+                        </div>
+                        <div class="integrations__item-desc">
+                            Yandex.Metrika assistant. Regular and requested reports.
+                        </div>
+                        <div class="integrations__item-footer">
+                             <div class="integrations__item-contributers">
+                                <div class="integrations__item-dev"></div>
+                                <div class="integrations__item-dev"></div>
+                                <div class="integrations__item-dev"></div>
+                            </div>
+                            <a class="integrations__item-link" href="">github.com/codex-bot/metrika</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
