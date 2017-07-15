ef998933fa6e997b0aa37e0303ea2cf3e5cec3c3
Polina Shneider <beatrixlinkoln@gmail.com>
2017-07-15 18:37:52 +0300

    Landing update
    Replaced .png with .svg
    Fixed folder structure
    Shadow, hover bg and margin fixes

--- application/views/templates/landings/codex_bot.php
+++ application/views/templates/landings/codex_bot.php
@@ -1,10 +1,10 @@
-<link rel="stylesheet" href="/public/app/landings/bot/codex_bot.css?v=<?= filemtime("public/app/landings/bot/codex_bot.css") ?>">
+<link rel="stylesheet" href="/public/app/landings/codex_bot/codex_bot.css?v=<?= filemtime("public/app/landings/codex_bot/codex_bot.css") ?>">
 <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
 
 <div class="center_side">
     <div class="codex-bot">
         <div class="codex-bot__about">
-        <img class="codex-bot__logo" src="/public/img/landings/bot/codex_bot_logo.png"> 
+        <img class="codex-bot__logo" src="/public/app/landings/codex_bot/img/Codex.svg"> 
             <div class="codex-bot__description">
                 <h2 class="codex-bot__info">Working team assistant</h2>
                 <div class="codex-bot__name">@codex_bot</div>
@@ -16,11 +16,11 @@
             </div>
             <div class="codex-bot__button-layout">
                 <a class="codex-bot__button button" href="">
-                    <img class="codex-bot__button-img" src="/public/img/landings/bot/slack_cmyk.png">
+                    <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/Slack.svg">
                     <span class="codex-bot__button-text">Add to <b>Slack</b></span>
                 </a>
                 <a class="codex-bot__button button" href="">
-                    <img class="codex-bot__button-img" src="/public/img/landings/bot/TelegramLogo2.png">
+                    <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/Telegram.svg">
                     <span class="codex-bot__button-text">Add to <b>Telegram</b></span>
                 </a>
             </div>
@@ -31,7 +31,7 @@
         </div>
         <div class="codex-bot__integrations-layout">
             <div class="integrations__block">
-                <img class="integrations__block-logo" src="/public/img/landings/bot/github.png">
+                <img class="integrations__block-github" src="/public/app/landings/codex_bot/img/Github.svg">
                 <div class="integrations__block-content">
                     <div class="integrations__block-title">
                         <b>GitHub</b>
@@ -45,13 +45,13 @@
                         <div class="integrations__block-dev"></div>
                         <div class="integrations__block-dev"></div>
                     </div>
-                    <a class="integrations__block-link" href="">github.com/codex-bot/github</a>
+                    <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
                 </div>
             </div>
             <div class="integrations__block">
                 <div class="integrations__block-content">
                     <div class="integrations__block-title">
-                        <img class="integrations__block-yandex" src="/public/img/landings/bot/yandex_eng_logo@1,5x.png">
+                        <img class="integrations__block-yandex" src="/public/app/landings/codex_bot/img/yandex_eng_logo.svg">
                     </div>
                     <div class="integrations__block-desc">
                         Yandex.Metrika assistant. Regular and requested reports.
@@ -61,9 +61,10 @@
                         <div class="integrations__block-dev"></div>
                         <div class="integrations__block-dev"></div>
                     </div>
-                    <a class="integrations__block-link" href="">github.com/codex-bot/metrika</a>
+                    <a class="integrations__block-link" href="https://github.com/codex-bot/metrika">github.com/codex-bot/metrika</a>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
