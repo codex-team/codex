38f0d42ad7ec341efa0f9b91ceeea1fe783931b2
Polina Shneider <beatrixlinkoln@gmail.com>
2017-07-17 20:19:59 +0300

    Landing new design
    Also optimised SVG

--- application/views/templates/landings/codex_bot.php
+++ application/views/templates/landings/codex_bot.php
@@ -4,26 +4,19 @@
 <div class="center_side">
     <div class="codex-bot">
         <div class="codex-bot__about">
-        <img class="codex-bot__logo" src="/public/app/landings/codex_bot/img/Codex.svg"> 
-            <div class="codex-bot__description">
-                <h2 class="codex-bot__info">Working team assistant</h2>
-                <div class="codex-bot__name">@codex_bot</div>
-                <div class="codex-bot__links">
-                    <a class="codex-bot__links-item" href="">Getting Started </a>
-                    <a class="codex-bot__links-item" href="">Documentation</a>
-                    <a class="codex-bot__links-item" href="">View code</a>
-                </div>
-            </div>
-            <div class="codex-bot__button-layout">
-                <a class="codex-bot__button button" href="">
-                    <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/Slack.svg">
-                    <span class="codex-bot__button-text">Add to <b>Slack</b></span>
-                </a>
-                <a class="codex-bot__button button" href="">
-                    <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/Telegram.svg">
-                    <span class="codex-bot__button-text">Add to <b>Telegram</b></span>
-                </a>
-            </div>
+            
+        <h2 class="codex-bot__info">Working team assistant</h2>
+        <div class="codex-bot__name">@codex_bot</div>
+        <div class="codex-bot__buttons-wrapper">
+            <a class="codex-bot__button button" href="">
+                <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/slack.svg">
+                <span class="codex-bot__button-text">Add to <b>Slack</b></span>
+            </a>
+            <a class="codex-bot__button button" href="">
+                <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/telegram.svg">
+                <span class="codex-bot__button-text">Add to <b>Telegram</b></span>
+            </a>
+        </div>
         </div>
         <div class="codex-bot__integrations-title">
             Available integrations
@@ -31,40 +24,80 @@
         </div>
         <div class="codex-bot__integrations-layout">
             <div class="integrations__block">
-                <img class="integrations__block-github" src="/public/app/landings/codex_bot/img/Github.svg">
-                <div class="integrations__block-content">
-                    <div class="integrations__block-title">
-                        <b>GitHub</b>
-                    </div>
-                    <div class="integrations__block-desc">
-                        Notifications about new issues, pull-requests, reviews. And other helpfully stuff.
-                    </div>
-                    <div class="integrations__block-contributers">
-                        <div class="integrations__block-dev"></div>
-                        <div class="integrations__block-dev"></div>
-                        <div class="integrations__block-dev"></div>
-                        <div class="integrations__block-dev"></div>
-                    </div>
-                    <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
+                <img class="integrations__block-img integrations__block-github" src="/public/app/landings/codex_bot/img/github.svg">
+                <div class="integrations__block-title">
+                    <b>GitHub</b>
                 </div>
+                <div class="integrations__block-desc">
+                    Notifications about new issues, pull-requests, reviews. And other helpfully stuff.
+                </div>
+                <div class="integrations__block-contributers">
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                </div>
+                <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
             </div>
             <div class="integrations__block">
-                <div class="integrations__block-content">
-                    <div class="integrations__block-title">
-                        <img class="integrations__block-yandex" src="/public/app/landings/codex_bot/img/yandex_eng_logo.svg">
-                    </div>
-                    <div class="integrations__block-desc">
-                        Yandex.Metrika assistant. Regular and requested reports.
-                    </div>
-                     <div class="integrations__block-contributers">
-                        <div class="integrations__block-dev"></div>
-                        <div class="integrations__block-dev"></div>
-                        <div class="integrations__block-dev"></div>
-                    </div>
-                    <a class="integrations__block-link" href="https://github.com/codex-bot/metrika">github.com/codex-bot/metrika</a>
+                <img class="integrations__block-img integrations__block-yandex" src="/public/app/landings/codex_bot/img/yandex_eng_logo.svg">
+                <div class="integrations__block-title">
+                    <b>Yandex.Metrika</b>
+                </div>
+                <div class="integrations__block-desc">
+                    Yandex.Metrika assistant. Regular and requested reports.
+                </div>
+                <div class="integrations__block-contributers">
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                </div>
+                <a class="integrations__block-link" href="https://github.com/codex-bot/metrika">github.com/codex-bot/metrika</a>
+            </div>
+            <div class="integrations__block">
+                <img class="integrations__block-img integrations__block-webhooks" src="/public/app/landings/codex_bot/img/webhooks.svg">
+                <div class="integrations__block-title">
+                    <b>Webhooks</b>
+                </div>
+                <div class="integrations__block-desc">
+                    Easy-to-setup notifications scheme. You will get special link that allows to send messages via simple POST requests.
+                </div>
+                <div class="integrations__block-contributers">
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                    <div class="integrations__block-dev"></div>
+                </div>
+                <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
+            </div>
+            <div class="integrations__block">
+                <img class="integrations__block-img integrations__block-your-own" src="/public/app/landings/codex_bot/img/your_own.svg">
+                <div class="integrations__block-title">
+                    <b>Your own integration</b>
+                </div>
+                <div class="integrations__block-desc">
+                    Learn how to create application with our SDK for Python.
+                </div>
+                <div class="integrations__read-guide">
+                    Read guide »
                 </div>
             </div>
-
+        </div>
+        <div class="codex-bot__text-block">
+            <div class="text-block__title">User Guide</div>
+            <div class="text-block__info">Read instructions about @codex_bot usage and building own applications with community.</div>
+            <div class="codex-bot__links">
+                <a class="codex-bot__links-item" href="">Getting Started </a>
+                <span class="codex-bot__links-delimeter">•</span>
+                <a class="codex-bot__links-item" href="">Documentation</a>
+                <span class="codex-bot__links-delimeter">•</span>
+                <a class="codex-bot__links-item" href="">View code</a>
+            </div>
+        </div>
+        <div class="codex-bot__text-block">
+            <div class="text-block__title">Contributors community</div>
+            <div class="text-block__info">We really appreciate your help and participation with improving and building platform together. Please, leave a feedback, bug-reports and add your own stuff.</div>
+            <a class="text-block__link" href="github.com/codex_bot">github.com/codex_bot</a>
         </div>
     </div>
 </div>
