419681855ba315b4b7d441b6cb9cae83b9f47a4d
Peter Savchenko <specc.dev@gmail.com>
2017-07-19 22:14:59 +0300

    Landing update. Not finished

--- application/views/templates/landings/codex_bot.php
+++ application/views/templates/landings/codex_bot.php
@@ -1,103 +1,127 @@
 <link rel="stylesheet" href="/public/app/landings/codex_bot/codex_bot.css?v=<?= filemtime("public/app/landings/codex_bot/codex_bot.css") ?>">
 <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
 
-<div class="center_side">
-    <div class="codex-bot">
-        <div class="codex-bot__about">
-            
+<div class="codex-bot center_side">
+
+    <div class="codex-bot__about">
         <h2 class="codex-bot__info">Working team assistant</h2>
-        <div class="codex-bot__name">@codex_bot</div>
-        <div class="codex-bot__buttons-wrapper">
-            <a class="codex-bot__button button" href="">
-                <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/slack.svg">
-                <span class="codex-bot__button-text">Add to <b>Slack</b></span>
+        <h1 class="codex-bot__name">@codex_bot</h2>
+        <a class="codex-bot__button" href="https://slack.com/oauth/authorize?&client_id=12842278998.198477752209&scope=incoming-webhook,bot,channels:history,channels:read,commands,chat:write:bot,emoji:read,groups:read,im:history,team:read,users:read,files:read,pins:read,groups:history,groups:write,mpim:history,mpim:read,mpim:write,channels:write">
+            <? include(DOCROOT . 'public/app/landings/codex_bot/img/slack-logo.svg'); ?>
+            Add to <b>Slack</b>
+        </a>
+        <a class="codex-bot__button" href="https://t.me/codex_bot">
+            <? include(DOCROOT . 'public/app/landings/codex_bot/img/telegram-logo.svg'); ?>
+            Add to <b>Telegram</b>
+        </a>
+    </div>
+
+    <h3 class="codex-bot__heading">
+        Available integrations
+    </h3>
+
+    <?
+        $apps = array(
+            array(
+                'name' => 'GitHub',
+                'description' => 'Notifications about new issues, pull-requests, reviews. And other helpfully stuff.',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'n0str', 'photo' => 'https://avatars1.githubusercontent.com/u/988885?v=4&s=60' ),
+                    array( 'name' => 'gohabereg', 'photo' => 'https://avatars3.githubusercontent.com/u/23050529?v=4&s=60' ),
+                ),
+                'url' => 'github.com/codex-bot/github',
+                'icon' => 'public/app/landings/codex_bot/img/github-logo.svg'
+            ),
+            array(
+                'name' => 'Yandex.Metrika',
+                'description' => 'Yandex.Metrika assistant. Regular and requested reports.',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'gohabereg', 'photo' => 'https://avatars3.githubusercontent.com/u/23050529?v=4&s=60' ),
+                ),
+                'url' => 'github.com/codex-bot/metrika',
+                'icon' => 'public/app/landings/codex_bot/img/yandex-logo.svg'
+            ),
+            array(
+                'name' => 'Webhooks',
+                'description' => 'Easy-to-setup notifications scheme. You will get special link that allows to send messages via simple POST requests.',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'gohabereg', 'photo' => 'https://avatars3.githubusercontent.com/u/23050529?v=4&s=60' ),
+                ),
+                'url' => 'github.com/codex-bot/notify',
+                'icon' => 'public/app/landings/codex_bot/img/webhooks.svg'
+            )
+
+        )
+    ?>
+
+    <div class="codex-bot__integrations-layout">
+
+        <? foreach ( $apps as $app ): ?>
+
+            <div class="bot-app">
+                <a class="bot-app__icon" href="//<?= $app['url'] ?>">
+                    <? include(DOCROOT . $app['icon']); ?>
+                </a>
+                <a class="bot-app__title" href="//<?= $app['url'] ?>">
+                    <?= $app['name']; ?>
+                </a>
+                <div class="bot-app__desc">
+                    <?= $app['description']; ?>
+                </div>
+
+                <div class="bot-app__contributors">
+                    <? foreach ( $app['contributors'] as $contributor ): ?>
+                        <a class="bot-app__contributor" href="//github.com/<?= $contributor['name']; ?>" title="<?= $contributor['name'] ?>">
+                            <img src="<?= $contributor['photo'] ?>" alt="<?= $contributor['name'] ?>">
+                        </a>
+                    <? endforeach; ?>
+                </div>
+
+                <a class="bot-app__link" href="//<?= $app['url'] ?>">
+                    <?= $app['url'] ?>
+                </a>
+            </div>
+
+        <? endforeach; ?>
+
+         <div class="bot-app">
+            <span class="bot-app__icon">
+                <? include(DOCROOT . 'public/app/landings/codex_bot/img/your_own.svg'); ?>
+            </span>
+            <img src="">
+            <div class="bot-app__title">
+                Your own integration
+            </div>
+            <div class="bot-app__desc">
+                Learn how to create application with our SDK for Python.
+            </div>
+            <a class="bot-app__link" href="//<?= $app['url'] ?>">
+                Read guide »
             </a>
-            <a class="codex-bot__button button" href="">
-                <img class="codex-bot__button-img" src="/public/app/landings/codex_bot/img/telegram.svg">
-                <span class="codex-bot__button-text">Add to <b>Telegram</b></span>
-            </a>
         </div>
-        </div>
-        <div class="codex-bot__integrations-title">
-            Available integrations
-            <a class="codex-bot__integrations-link" href="">How to create your own</a>
-        </div>
-        <div class="codex-bot__integrations-layout">
-            <div class="integrations__block">
-                <img class="integrations__block-img integrations__block-github" src="/public/app/landings/codex_bot/img/github.svg">
-                <div class="integrations__block-title">
-                    <b>GitHub</b>
-                </div>
-                <div class="integrations__block-desc">
-                    Notifications about new issues, pull-requests, reviews. And other helpfully stuff.
-                </div>
-                <div class="integrations__block-contributers">
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                </div>
-                <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
-            </div>
-            <div class="integrations__block">
-                <img class="integrations__block-img integrations__block-yandex" src="/public/app/landings/codex_bot/img/yandex_eng_logo.svg">
-                <div class="integrations__block-title">
-                    <b>Yandex.Metrika</b>
-                </div>
-                <div class="integrations__block-desc">
-                    Yandex.Metrika assistant. Regular and requested reports.
-                </div>
-                <div class="integrations__block-contributers">
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                </div>
-                <a class="integrations__block-link" href="https://github.com/codex-bot/metrika">github.com/codex-bot/metrika</a>
-            </div>
-            <div class="integrations__block">
-                <img class="integrations__block-img integrations__block-webhooks" src="/public/app/landings/codex_bot/img/webhooks.svg">
-                <div class="integrations__block-title">
-                    <b>Webhooks</b>
-                </div>
-                <div class="integrations__block-desc">
-                    Easy-to-setup notifications scheme. You will get special link that allows to send messages via simple POST requests.
-                </div>
-                <div class="integrations__block-contributers">
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                    <div class="integrations__block-dev"></div>
-                </div>
-                <a class="integrations__block-link" href="https://github.com/codex-bot/github">github.com/codex-bot/github</a>
-            </div>
-            <div class="integrations__block">
-                <img class="integrations__block-img integrations__block-your-own" src="/public/app/landings/codex_bot/img/your_own.svg">
-                <div class="integrations__block-title">
-                    <b>Your own integration</b>
-                </div>
-                <div class="integrations__block-desc">
-                    Learn how to create application with our SDK for Python.
-                </div>
-                <div class="integrations__read-guide">
-                    Read guide »
-                </div>
-            </div>
-        </div>
-        <div class="codex-bot__text-block">
-            <div class="text-block__title">User Guide</div>
-            <div class="text-block__info">Read instructions about @codex_bot usage and building own applications with community.</div>
-            <div class="codex-bot__links">
-                <a class="codex-bot__links-item" href="">Getting Started </a>
-                <span class="codex-bot__links-delimeter">•</span>
-                <a class="codex-bot__links-item" href="">Documentation</a>
-                <span class="codex-bot__links-delimeter">•</span>
-                <a class="codex-bot__links-item" href="">View code</a>
-            </div>
-        </div>
-        <div class="codex-bot__text-block">
-            <div class="text-block__title">Contributors community</div>
-            <div class="text-block__info">We really appreciate your help and participation with improving and building platform together. Please, leave a feedback, bug-reports and add your own stuff.</div>
-            <a class="text-block__link" href="github.com/codex_bot">github.com/codex_bot</a>
+
+    </div>
+    <div class="codex-bot__text-block">
+        <div class="text-block__title">User Guide</div>
+        <div class="text-block__info">Read instructions about @codex_bot usage and building own applications with community.</div>
+        <div class="codex-bot__links">
+            <a class="codex-bot__links-item" href="">Getting Started </a>
+            <span class="codex-bot__links-delimeter">•</span>
+            <a class="codex-bot__links-item" href="">Documentation</a>
+            <span class="codex-bot__links-delimeter">•</span>
+            <a class="codex-bot__links-item" href="">View code</a>
         </div>
     </div>
+    <div class="codex-bot__text-block">
+        <div class="text-block__title">Contributors community</div>
+        <div class="text-block__info">We really appreciate your help and participation with improving and building platform together. Please, leave a feedback, bug-reports and add your own stuff.</div>
+        <a class="text-block__link" href="github.com/codex_bot">github.com/codex_bot</a>
+    </div>
+
 </div>
