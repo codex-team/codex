da35f06a6de46abe30895311bf60ca368e67c810
Taly Guryn <vitalik7tv@yandex.ru>
2017-07-27 14:40:22 +0300

    concating strings in php with dots

--- application/views/templates/articles/article.php
+++ application/views/templates/articles/article.php
@@ -46,7 +46,7 @@
         <div class="article-info">
             <div class="article-info__author" itemscope itemtype="http://schema.org/Person" itemprop="author">
 
-                <meta itemprop="url" href="/<?= $article->author->uri ? : 'user/' + $article->author->id ?>" />
+                <meta itemprop="url" href="/<?= $article->author->uri ? : 'user/' . $article->author->id ?>" />
 
                 <time class="article-info__date"><?= Date::fuzzy_span(strtotime($article->dt_create)) ?></time>
                 <img class="article-info__photo" src="<?= $article->author->photo ?>" alt="/<?= $article->author->name ?>"  itemprop="image">
