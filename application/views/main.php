062b8feb2a3765a35c5cc5acbe766c1c0b4f51e6
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-23 19:31:05 +0300

    Apple icon

--- application/views/main.php
+++ application/views/main.php
@@ -19,6 +19,9 @@
     <link rel="stylesheet" href="/public/build/bundle.css?v=<?= filemtime('public/build/bundle.css') ?>">
     <link rel="icon" type="image/png" href="/public/app/img/fav_shield@3x.png?v=985" id="favicon" />
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+
     <meta id="metaImage" name="image" property="og:image"  content="https://ifmo.su/public/app/img/meta_img.png" />
     <link id="linkImage" rel="image_src" href="https://ifmo.su/public/app/img/meta_img.png" />
 
