fe79f1b3b3921e138e3dc530341a3bba0da3e0b1
Peter Savchenko <specc.dev@gmail.com>
2017-11-30 19:19:55 +0300

    news translated

--- application/views/templates/index/news.php
+++ application/views/templates/index/news.php
@@ -2,117 +2,145 @@
     $news = array(
         array(
             'text' => 'Вышла статья о том, <a href="/viber-bot">как создать бота для Viber</a>',
+            'textEn' => 'Published a new note «<a href="/viber-bot">How to create a bot for Viber</a>»',
             'date' => '2 nov'
         ),
         array(
             'text' => 'Провели мастер-класс по <a href="https://vk.com/wall-103229636_326">основам модульной Frontend-разработки</a>',
+            'textEn' => 'CodeX Meetup: <a href="https://vk.com/wall-103229636_326">modular Frontend</a>',
             'date' => '25 oct'
         ),
         array(
             'text' => 'Опубликованы <a href="/task">задания для вступающих в клуб</a>',
+            'textEn' => '<a href="/task">Tasks for joining the Team</a> are published',
             'date' => '1 oct'
         ),
         array(
             'text' => 'Набросали гайд о том, <a href="/ssh-passwordless-login">как настроить беспарольный доступ по SSH</a>',
+            'textEn' => 'Published a new guide about <a href="/ssh-passwordless-login">how to set up a passwrodless SSH access</a>',
             'date' => '20 sep'
         ),
         array(
             'text' => 'Написали о том, что такое <a href="/jwt">JSON Web Token</a>',
+            'textEn' => 'Read our article about <a href="/jwt">JSON Web Token</a>',
             'date' => '15 sep'
         ),
         array(
             'text' => 'Рассказали об опыте <a href="https://vk.com/codex_team?w=wall-103229636_291">работы с дополненной реальностью на ARKit</a>',
+            'textEn' => 'Tell about out experience <a href="https://vk.com/codex_team?w=wall-103229636_291">of working with Augmented Reality with Apple ARKit</a>',
             'date' => '12 sep'
         ),
         array(
             'text' => 'Обзор обновления <a href="/github-codeowners">GitHub Code Owners</a>',
+            'textEn' => 'Overview of <a href="/github-codeowners">GitHub Code Owners</a>',
             'date' => '2 aug'
         ),
         array(
             'text' => 'Рассказали о том, <a href="/slack-apps-tutorial">как создать приложение для Slack</a>',
+            'textEn' => 'New guide «<a href="/slack-apps-tutorial">How to create an applicaition for Slack</a>»',
             'date' => '24 jul'
         ),
         array(
             'text' => 'Написали статью о создании <a href="/python-sockets">сокет сервера</a> на Python',
+            'textEn' => 'New article about development <a href="/python-sockets">Socket-server</a> with Python',
             'date' => '4 may'
         ),
         array(
             'text' => 'Опубликовали в NPM <a href="https://www.npmjs.com/package/codex.editor.personality">плагин «Персона»</a> для CodeX Editor',
+            'textEn' => 'New plugin «<a href="https://www.npmjs.com/package/codex.editor.personality">Personality</a>» for the CodeX Editor was published on NPM',
             'date' => '25 apr',
             'release' => true
         ),
         array(
             'text' => 'Представляем CodeX Media — <a href="/media">платформу для создания UGC-медиа</a>',
+            'textEn' => 'Meet CodeX Media — <a href="/media">Platfrom for building UGC-media</a>',
             'date' => '24 apr',
             'release' => true
         ),
         array(
             'text' => 'Вышла статья от Университета ИТМО о встрече <a href="http://news.ifmo.ru/ru/startups_and_business/initiative/news/6604/">CodeX Meetup: Docker</a>',
+            'textEn' => 'Story about <a href="http://news.ifmo.ru/en/startups_and_business/initiative/news/6604/">CodeX Meetup: Docker</a>',
             'date' => '21 apr'
         ),
         array(
             'text' => 'Написали о <a href="/webpack-tutorial">сборке JavaScript модулей</a> с помощью Webpack',
+            'textEn' => 'Wrote about <a href="/webpack-tutorial">bundling JavaScript-components</a> with Webpack',
             'date' => '15 apr'
         ),
         array(
             'text' => '<a href="https://vk.com/codex_team?w=wall-103229636_251">CodeX Meetup: Webpack</a>',
+            'textEn' => '<a href="https://vk.com/codex_team?w=wall-103229636_251">CodeX Meetup: Webpack</a>',
             'date' => '14 apr'
         ),
         array(
             'text' => 'Написали о том, <a href="/mysql-and-emoji">как подружить базу данных с Emoji</a> 😈',
+            'textEn' => 'Dropped a few lines about <a href="/mysql-and-emoji">using Emoji in MySQL</a> 😈',
             'date' => '1 apr'
         ),
         array(
             'text' => 'Поделились опытом использования Docker на <a href="https://vk.com/spbifmo?w=wall-94_31958">CodeX Meetup:Docker</a>',
+            'textEn' => 'Shared our experience about using a Docker at the <a href="https://vk.com/spbifmo?w=wall-94_31958">CodeX Meetup:Docker</a>',
             'date' => '31 mar'
         ),
         array(
             'text' => 'Вышла статья о прошедшем мастер-классе по верстке от «<a href="http://mbradio.ru/publication/1977/">Мегабайт Медиа</a>»',
+            'textEn' => 'Story about a past master class «CodeX Meetup: HTML+CSS practice» from <a href="http://mbradio.ru/publication/1977/">Megabyte Media</a>»',
             'date' => '29 mar'
         ),
         array(
             'text' => 'Написали простую инструкцию о том, <a href="https://ifmo.su/ssl">как бесплатно и просто настроить HTTPS с помощью Let\'s Encrypt</a>',
+            'textEn' => 'Wrote a simple guide about <a href="https://ifmo.su/ssl">how to set up and HTTPS website version with Let\'s Encrypt</a>',
             'date' => '22 mar'
         ),
         array(
             'text' => 'Прошел грандиозный <a href="https://github.com/codex-team/html-css-practice">CodeX Meetup: HTML+CSS practice</a>',
+            'textEn' => 'Left a grand <a href="https://github.com/codex-team/html-css-practice">CodeX Meetup: HTML+CSS practice</a>',
             'date' => '17 mar'
         ),
         array(
             'text' => 'Опубликовали статью о <a href="/macos-web-server">настройке окружения для веб-разработки на macOS</a>',
+            'textEn' => 'Published an entry about <a href="/macos-web-server">setting up an local environment for Web-development on macOS</a>',
             'date' => '13 mar'
         ),
         array(
             'text' => 'Помучили GPU и CPU на <a href="https://vk.com/codex_team?w=wall-103229636_232">CodeX Meetup: Chrome DevTools</a>',
+            'textEn' => 'Racked GPU and CPU at the <a href="https://vk.com/codex_team?w=wall-103229636_232">CodeX Meetup: Chrome DevTools</a>',
             'date' => '3 mar'
         ),
         array(
             'text' => 'Прошел первый открытый мастер-класс <a href="https://vk.com/codex_team?w=wall-103229636_229">CodeX Meetup</a>, посвященный основам разработки real-time приложений на node.js и WebSockets',
+            'textEn' => 'The first open master class <a href="https://vk.com/codex_team?w=wall-103229636_229">CodeX Meetup</a>, about real-time application development with node.js and WebSockets',
             'date' => '17 feb'
         ),
         array(
             'text' => 'Вышла новая статья о <a href="/js-modules">модульной разработке в JavaScript</a>',
+            'textEn' => 'Published a new article about <a href="/js-modules">first steps in JavaScript Modules</a>',
             'date' => '31 dec'
         ),
         array(
             'text' => 'Опубликовали статью о том, <a href="/docker-php">как использовать Docker-контейнер</a>',
+            'textEn' => 'Writo a guide about <a href="/docker-php">usage of Docker-containers</a>',
             'date' => '1 dec'
         ),
         array(
-            'text' => 'Вышла <a href="http://news.ifmo.ru/ru/science/it/news/6243/">статья о клубе</a> от редакции НИУ ИТМО',
+            'text' => 'Вышла <a href="http://news.ifmo.ru/ru/science/it/news/6243/">статья о клубе</a> от редакции Университета ИТМО',
+            'textEn' => 'Published <a href="http://news.ifmo.ru/en/science/it/news/6243/">story about CodeX Team</a> from editoral office of IFMO University',
             'date' => '30 nov'
         ),
         array(
             'text' => 'Опубликованы <a href="/task">задания для вступающих в клуб</a>',
+            'textEn' => 'Tasks for <a href="/task">joining the Team</a>',
             'date' => '23 oct'
         ),
         array(
             'text' => 'Модуль для создания контрастной версии сайта <a href="/special">CodeX Special</a>',
+            'textEn' => 'Module for cration high-contrast version of websites <a href="/special">CodeX Special</a>',
             'date' => '18 oct',
             'release' => true,
         ),
         array(
             'text' => 'Открыт <a href="/join">набор в клуб</a>',
+            'textEn' => 'Opened <a href="/join">invite to the Team</a>',
             'date' => '4 oct'
         ),
         // array(
@@ -122,23 +150,28 @@
         // ),
         array(
             'text' => '<a href="/bot">@codex_bot</a> — Облачная платформа для интеграции сервисов в Telegram. Модули по работе с GitHub и Yandex.Metrika',
+            'textEn' => '<a href="/bot">@codex_bot</a> — Cloud platform for integration services into messangers. Apps for GitHub and Yandex.Metrika',
             'date' => '22 sep',
             'release' => true,
         ),
         array(
             'text' => 'Новая статья: «<a href="/alias-system">Система алиасов</a>»',
+            'textEn' => 'New article: «<a href="/alias-system">Alias system</a>»',
             'date' => '15 jul'
         ),
         array(
             'text' => 'Написали о <a href="/metrika-telegram">публикации статистики из Яндекс.Метрики в Telegram</a>',
+            'textEn' => 'Write about <a href="/metrika-telegram">publication analytics data from the Yandex.Metrica to the Telegram messanger</a>',
             'date' => '27 jul'
         ),
         array(
             'text' => 'Статья о <a href="/supervisor">перезапуске скриптов с помощью модуля Supervisor</a>',
+            'textEn' => 'Published a guide about <a href="/supervisor">script control system with Supervisor</a>',
             'date' => '24 mar'
         ),
         array(
             'text' => 'Новая статья: «<a href="/scala-tutorial">Разработка на Scala: первые шаги</a>»',
+            'textEn' => 'Meet a new «<a href="/scala-tutorial">beginners guide about first steps in Scala</a>»',
             'date' => '19 mar'
         ),
         // array(
@@ -162,7 +195,11 @@
                 <? if (!empty($event['release'])): ?>
                     <span class="news__bage">release</span>
                 <? endif ?>
-                <?= $event['text'] ?>
+                <? if (!empty($event['textEn'])): ?>
+                    <?= $event['textEn'] ?>
+                <? else: ?>
+                    <?= $event['text'] ?>
+                <? endif ?>
             </li>
             <? $i++; ?>
         <? endforeach; ?>
