d232b80456cb7559abfc3914f83c16a2a8f7b1f8
dankunis <dankunis@github.com>
2017-12-14 21:21:47 +0300

    refactoring

--- application/classes/i18n-setup.php
+++ application/classes/i18n-setup.php
@@ -9,106 +9,106 @@
 class Internationalization {
 
 
-  /**
-   * Supported languages
-   */
-  private $langsSupported = array(
-    'en' => 'en_US',
-    'ru' => 'ru_RU',
-  );
+    /**
+     * Supported languages
+     */
+    private $langsSupported = array(
+        'en' => 'en_US',
+        'ru' => 'ru_RU',
+    );
 
-  /**
-   * Name of your .mo and .po files
-   * (usually named after your domain)
-   */
-  private $domain = 'ifmo.su';
+    /**
+     * Name of your .mo and .po files
+     * (usually named after your domain)
+     */
+    private $domain = 'ifmo.su';
 
-  /**
-   * Directory for gettext translations
-   */
-  private $localedir = DOCROOT . 'locale';
+    /**
+     * Directory for gettext translations
+     */
+    private $localedir = DOCROOT . 'locale';
 
-  /**
-   * Chosen language from the GET form
-   */
-  private $getLang;
+    /**
+     * Chosen language from the GET form
+     */
+    private $getLang;
 
-  /**
-   * Preferable language that is stored in COOKIES
-   */
-  private $cookieLang;
+    /**
+     * Preferable language that is stored in COOKIES
+     */
+    private $cookieLang;
 
-  /**
-   * Language used to configure Gettext
-   */
-  private $lang;
+    /**
+     * Language used to configure Gettext
+     */
+    private $lang;
 
 
-  /**
-   * Calls other functions to setup Gettext
-   * @param string $defaultLang
-   */
-  function __construct($defaultLang='en')
-  {
-    if (isset($_GET['lang'])) {
-      $this->getLang = $_GET['lang'];
+    /**
+     * Calls other functions to setup Gettext
+     * @param string $defaultLang
+     */
+    function __construct($defaultLang = 'en')
+    {
+        if (isset($_GET['lang'])) {
+            $this->getLang = $_GET['lang'];
+        }
+
+        if (isset($_COOKIE['lang'])) {
+            $this->cookieLang = $_COOKIE['lang'];
+        }
+
+        $this->lang = $this->langsSupported[$defaultLang];
+        $this->langSetup();
+        $this->envSetup();
     }
 
-    if (isset($_COOKIE['lang'])) {
-      $this->cookieLang = $_COOKIE['lang'];
+
+    /**
+     * Verifies if the given language is supported in the project
+     * @param string $locale
+     * @return bool
+     */
+    private function valid($locale)
+    {
+        return array_key_exists($locale, $this->langsSupported);
     }
 
-    $this->lang = $this->langsSupported[$defaultLang];
-    $this->langSetup();
-    $this->envSetup();
-  }
+    /**
+     * Chooses the prefurable language
+     */
+    private function langSetup()
+    {
+        // Check whether the user used the GET form
+        if (isset($this->getLang) && $this->valid($this->getLang)) {
 
+            // Store user's choice
+            setcookie('lang', $this->getLang);
+            $this->lang = $this->langsSupported[$this->getLang];
 
-  /**
-   * Verifies if the given language is supported in the project
-   * @param string $locale
-   * @return bool
-   */
-  private function valid($locale)
-  {
-    return array_key_exists($locale, $this->langsSupported);
-  }
+        } elseif (isset($this->cookieLang) && $this->valid($this->cookieLang)) {
 
-  /**
-   * Chooses the prefurable language
-   */
-  private function langSetup()
-  {
-    // Check whether the user used the GET form
-    if (isset($this->getLang) && $this->valid($this->getLang)) {
-
-      // Store user's choice
-      setcookie('lang', $this->getLang);
-      $this->lang = $this->langsSupported[$this->getLang];
-
-    } elseif (isset($this->cookieLang) && $this->valid($this->cookieLang)) {
-
-      // Choose language based on user's previous choice
-      $this->lang = $this->langsSupported[$this->cookieLang];
+            // Choose language based on user's previous choice
+            $this->lang = $this->langsSupported[$this->cookieLang];
+        }
     }
-   }
 
-   /**
-   * Configures gettext
-   */
-  private function envSetup()
-  {
-    // Set $lang as value of the environment variable 'LANG'
-    putenv('LANG=' . $this->lang);
-    setlocale(LC_ALL, $this->lang);
+     /**
+     * Configures gettext
+     */
+    private function envSetup()
+    {
+        // Set $lang as value of the environment variable 'LANG'
+        putenv('LANG=' . $this->lang);
+        setlocale(LC_ALL, $this->lang);
 
-    // Set path to the $domain.po and $domain.mo files
-    bindtextdomain($this->domain, $this->localedir);
+        // Set path to the $domain.po and $domain.mo files
+        bindtextdomain($this->domain, $this->localedir);
 
-    // Specify the character encoding in which the messages from the $domain message catalog will be returned
-    bind_textdomain_codeset($this->domain, 'UTF-8');
+        // Specify the character encoding in which the messages from the $domain message catalog will be returned
+        bind_textdomain_codeset($this->domain, 'UTF-8');
 
-    // Set the defualt domain where gettext() will search for the translations
-    textdomain($this->domain);
-  }
+        // Set the defualt domain where gettext() will search for the translations
+        textdomain($this->domain);
+    }
 }
