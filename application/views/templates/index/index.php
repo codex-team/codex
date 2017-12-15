c4eaa1846e7b16080b9a4986fbe9773fda56815b
dankunis <dankunis@github.com>
2017-12-15 12:57:58 +0300

    updated texts on main page

--- application/views/templates/index/index.php
+++ application/views/templates/index/index.php
@@ -28,10 +28,10 @@
         <div class="codex-logo"></div>
         <p>
             <?=
-                    _("CodeX is a new team based in ITMO University, unifying students and graduates interested in web-development, design and studying new technologies in practise.");
+                    _("CodeX is a new team based in ITMO University, unifying students and graduates interested in web-development, design and studying new technologies in practice.");
             ?>
         </p>
-        <p><?= _("Our goal — make a team with fire in the eyes and idealistic tempers.") ?></p>
+        <p><?= _('Our goal — make team with fire in eyes and idealistic tempers.') ?></p>
 
         <? /** Join button */ ?>
         <? /* <br><a class="button button--green index-page__join-button" href="/join">Вступить в клуб</a> */ ?>
