b8cec6f694ec89677e31b0ab29d0f116abbcc119
Taly <vitalik7tv@yandex.ru>
2018-02-13 07:48:52 +0300

    change table name: articles -> Articles

--- migrations/2018-01-30_add_article_text_en.sql
+++ migrations/2018-01-30_add_article_text_en.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `articles` DROP `text`;
+ALTER TABLE `Articles` DROP `text`;
 
 ALTER TABLE `Articles` CHANGE `json` `text` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
 
-ALTER TABLE `articles` ADD `linked_article` INT(10) NULL DEFAULT NULL AFTER `uri`;
+ALTER TABLE `Articles` ADD `linked_article` INT(10) NULL DEFAULT NULL AFTER `uri`;
 
-ALTER TABLE `articles` ADD `lang` VARCHAR(128) NULL DEFAULT NULL AFTER `description`;
+ALTER TABLE `Articles` ADD `lang` VARCHAR(128) NULL DEFAULT NULL AFTER `description`;
