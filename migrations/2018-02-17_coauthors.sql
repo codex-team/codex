839ea6dd851cfaef07ad76c660da185303687435
Polina Shneider <beatrixlinkoln@gmail.com>
2018-02-25 00:47:27 +0300

    Oups, migrations update 🤪

--- migrations/2018-02-17_coauthors.sql
+++ migrations/2018-02-17_coauthors.sql
@@ -1,3 +1,3 @@
-CREATE TABLE `codexdb`.`coauthors` ( `article_id` INT(10) UNSIGNED NOT NULL , `user_id` INT(10) NULL DEFAULT NULL ) ENGINE = InnoDB;
-ALTER TABLE `coauthors` ADD PRIMARY KEY(`article_id`);
-ALTER TABLE `coauthors` ADD INDEX(user_id);
+CREATE TABLE `Coauthors` ( `article_id` INT(10) UNSIGNED NOT NULL , `user_id` INT(10) NULL DEFAULT NULL ) ENGINE = InnoDB;
+ALTER TABLE `Coauthors` ADD PRIMARY KEY(`article_id`);
+ALTER TABLE `Coauthors` ADD INDEX(user_id);
