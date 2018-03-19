4bf18b52dba1ae269d101136c6186dc74a7c5671
Polina Shneider <beatrixlinkoln@gmail.com>
2018-03-20 00:09:50 +0300

    Make separate component

--- application/views/templates/articles/list_wrapper.php
+++ application/views/templates/articles/list_wrapper.php
@@ -4,19 +4,10 @@
 
         <div class="site-section site-section--articles-list">
             <h2 class="site-section__title">Our articles</h2>
-            <div class="site-section__desc">We are writing about our experience and researches.</div>
+            <div class="site-section__desc">We write about our experience and researches.</div>
         </div>
 
-        <div class="follow-us">
-            <div class="follow-us__contents">
-                <h4 class="follow-us__title">CodeX</h4>
-                <div class="follow-us__desc">Notes about web-dev and some interesting IT insights in the our lovely messenger</div>
-            </div>
-            <a class="follow-us__button" href="//telegram.me/codex_team" target="_blank">
-                <img src="/public/app/img/icon_telegram_white.svg">
-                Follow @codex_team
-            </a>
-        </div>
+        <?= View::factory('templates/blocks/follow_telegram'); ?>
 
     </div>
 
---
+++ application/views/templates/blocks/follow_telegram.php
@@ -0,0 +1,10 @@
+<div class="follow-telegram">
+    <div class="follow-telegram__contents">
+        <h4 class="follow-telegram__title">CodeX</h4>
+        <div class="follow-telegram__desc">Notes about web-dev and some interesting IT insights in the our lovely messenger</div>
+    </div>
+    <a class="follow-telegram__button" href="//telegram.me/codex_team" target="_blank">
+    	<? include(DOCROOT . "public/app/img/icon_telegram_white.svg") ?>
+        Follow @codex_team
+    </a>
+</div>
