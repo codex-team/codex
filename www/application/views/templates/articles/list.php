69e34fe2aac6c280b7aff1cd5f5738681bba17ec
Polina Shneider <beatrixlinkoln@gmail.com>
2018-05-28 17:55:22 +0300

    Fix HTML::chars issue

--- www/application/views/templates/articles/list.php
+++ www/application/views/templates/articles/list.php
@@ -1,24 +1,24 @@
 <div class="feed">
     <? foreach ($feed_items as $i => $item): ?>
 
-        <article class="feed-item clearfix <?= $item->marked ? 'feed-item--big' : ''?> <?= $item->cover ? 'feed-item--with-cover' : '' ?><?= $item->is_big_cover ? ' feed-item--with-big-cover' : ''?>" data-type="<?= $item::FEED_PREFIX; ?>" data-id="<?= $item->id; ?>">
+        <article class="feed-item clearfix <?= $item->marked ? 'feed-item--big' : ''?> <?= $item->cover ? 'feed-item--with-cover' : '' ?><?= $item->is_big_cover ? ' feed-item--with-big-cover' : ''?>" data-type="<?= $item::FEED_PREFIX; ?>" data-id="<?= HTML::chars($item->id); ?>">
 
             <?
                 $url = '';
-                if (HTML::chars($item::FEED_PREFIX == 'article')){
-                    $url = HTML::chars($item->uri) ?: 'article/' . HTML::chars($item->id);
+                if ($item::FEED_PREFIX == 'article'){
+                    $url = $item->uri ?: 'article/' . $item->id;
                 } else {
-                    $url = HTML::chars($item->uri) ?: 'course/' . HTML::chars($item->id);
+                    $url = $item->uri ?: 'course/' . $item->id;
                 }
             ?>
 
-            <? if (HTML::chars($item->cover)): ?>
-                <a class="feed-item__cover" href="<?= $url ?>">
-                    <img src="<?= $item->cover ?>">
+            <? if ($item->cover): ?>
+                <a class="feed-item__cover" href="<?= HTML::chars($url) ?>">
+                    <img src="<?= HTML::chars($item->cover) ?>">
                 </a>
             <? endif; ?>
 
-            <a class="feed-item__title js-emoji-included" href="/<?= $url  ?>">
+            <a class="feed-item__title js-emoji-included" href="/<?= HTML::chars($url)  ?>">
                 <?= HTML::chars($item->title) ?>
             </a>
 
@@ -31,20 +31,20 @@
                     <?= date_format(date_create($item->dt_publish), 'd M Y'); ?>
                 </time>
 
-                <? if (HTML::chars($item::FEED_PREFIX == 'article')): ?>
-                    <a class="feed-item__author-photo" href="/user/<?= $item->author->id ?>">
-                        <img src="<?= $item->author->photo ?>" alt="<?= $item->author->name ?>">
+                <? if ($item::FEED_PREFIX == 'article'): ?>
+                    <a class="feed-item__author-photo" href="/user/<?= HTML::chars($item->author->id) ?>">
+                        <img src="<?= HTML::chars($item->author->photo) ?>" alt="<?= HTML::chars($item->author->name) ?>">
                     </a>
-                    <? if (HTML::chars($item->coauthor->id)): ?>
-                        <a class="feed-item__author-photo" href="/user/<?= $item->coauthor->id ?>">
-                            <img src="<?= $item->coauthor->photo ?>" alt="<?= $item->coauthor->name ?>">
+                    <? if ($item->coauthor->id): ?>
+                        <a class="feed-item__author-photo" href="/user/<?= HTML::chars($item->coauthor->id) ?>">
+                            <img src="<?= HTML::chars($item->coauthor->photo) ?>" alt="<?= HTML::chars($item->coauthor->name) ?>">
                         </a>
                     <? endif; ?>
-                    <a class="feed-item__author-name" href="/user/<?= $item->author->id ?>">
+                    <a class="feed-item__author-name" href="/user/<?= HTML::chars($item->author->id) ?>">
                         <?= HTML::chars($item->author->name) ?>
                     </a>
                     <? if ($item->coauthor->id): ?>
-                        and <a class="feed-item__author-name" href="/user/<?= $item->coauthor->id ?>">
+                        and <a class="feed-item__author-name" href="/user/<?= HTML::chars($item->coauthor->id) ?>">
                             <?= HTML::chars($item->coauthor->name) ?>
                         </a>
                     <? endif; ?>
