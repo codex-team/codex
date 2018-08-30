b435c77f227c3beaaa1f82de762fbed850d273c5
Polina Shneider <polina@MacBook-Air.local>
2018-08-30 17:12:13 +0300

    Remove old landing styles

--- www/application/views/templates/editor/landing.php
+++ www/application/views/templates/editor/landing.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="/public/app/landings/editor/editor.css?v=<?= filemtime("public/app/landings/editor/editor.css") ?>">
 
-<article class="editor-landing editor-form article-content">
+<div class="editor-landing">
     <div class="editor-landing__info">
-        <h1 class="editor-landing__title js-emoji-included" itemprop="headline">CodeX Editor</h1>
+        <h1 class="editor-landing__title js-emoji-included">CodeX Editor</h1>
         <div class="editor-landing__description">Next generation block styled editor. Free. Use for pleasure.</div>
 
         <div class="editor-landing__links clearfix">
@@ -10,7 +10,7 @@
             <a href="#" class="editor-landing__links-item">Plugins</a>
         </div>
     </div>
-    <form data-module="editor" name="editor-demo" action="/editor/preview" method="POST" enctype="multipart/form-data">
+    <div class="editor-landing__demo" data-module="editor">
         <module-settings hidden>
             {
                 "blocks" : [
@@ -18,7 +18,7 @@
                         "type" : "header",
                         "data" : {
                             "text" : "Outputs clear data at JSON",
-                            "level" : 3
+                            "level" : 2
                         }
                     },
                     {
@@ -31,7 +31,7 @@
                         "type" : "header",
                         "data" : {
                             "text" : "API is the feature",
-                            "level" : 3
+                            "level" : 2
                         }
                     },
                     {
@@ -46,20 +46,15 @@
             }
         </module-settings>
 
-        <? //<textarea hidden name="html" id="codex_editor" cols="30" rows="10" style="width: 100%;height: 300px;"></textarea> ?>
         <div id="codex-editor"></div>
-        <textarea hidden name="article_json" id="json_result" cols="30" rows="10" style="width: 100%;height: 300px;"></textarea>
 
-        <div class="editor_output__buttons">
-            <div class="button button--master" id="saveButton">
+        <div class="editor-landing__button-wrapper">
+            <div class="button editor-landing__button" id="saveButton">
                 View Output
             </div>
         </div>
-
-    </form>
-
-</article>
-
-<div class="editor-output-preview">
-    <pre id="output"></pre>
+    </div>
+    <div class="editor-landing__preview">
+        <pre id="output"></pre>
+    </div>
 </div>
