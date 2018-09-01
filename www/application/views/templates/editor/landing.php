7b09c5ef70b0d3c6dc70a836475eeff25d1707f1
Polina Shneider <polina@MacBook-Air.local>
2018-09-01 20:32:34 +0300

    Add JavaScript Tools types filter

--- www/application/views/templates/editor/landing.php
+++ www/application/views/templates/editor/landing.php
@@ -15,40 +15,49 @@
             <a href="#" class="editor-landing__links-item">Plugins</a>
         </div>
     </div>
-    <div class="editor-landing__demo" data-module="editor">
+    <div class="editor-landing__demo" data-module="editor pluginsFilter">
         <module-settings hidden>
-            {
-                "blocks" : [
-                    {
-                        "type" : "header",
-                        "data" : {
-                            "text" : "Outputs clear data at JSON",
-                            "level" : 2
+            [
+                {
+                    "blocks" : [
+                        {
+                            "type" : "header",
+                            "data" : {
+                                "text" : "Outputs clear data at JSON",
+                                "level" : 2
+                            }
+                        },
+                        {
+                            "type" : "paragraph",
+                            "data" : {
+                                "text" : "Use it in Web, mobile, APM, Instant Articles, speech readers — everywhere."
+                            }
+                        },
+                        {
+                            "type" : "header",
+                            "data" : {
+                                "text" : "API is the feature",
+                                "level" : 2
+                            }
+                        },
+                        {
+                            "type" : "paragraph",
+                            "data" : {
+                                "text" : "Easy to build Plugins. Dozens of created."
+                            }
                         }
-                    },
-                    {
-                        "type" : "paragraph",
-                        "data" : {
-                            "text" : "Use it in Web, mobile, APM, Instant Articles, speech readers — everywhere."
-                        }
-                    },
-                    {
-                        "type" : "header",
-                        "data" : {
-                            "text" : "API is the feature",
-                            "level" : 2
-                        }
-                    },
-                    {
-                        "type" : "paragraph",
-                        "data" : {
-                            "text" : "Easy to build Plugins. Dozens of created."
-                        }
-                    }
-                ],
-                "button_id" : "saveButton",
-                "output_id" : "output"
-            }
+                    ],
+                    "button_id" : "saveButton",
+                    "output_id" : "output"
+                },
+                {
+                    "inlineFilterButtonClass" : ".js-inline-tools-filter",
+                    "blockFilterButtonClass" : ".js-block-tools-filter",
+                    "blockToolsClass" : ".js-block-tool",
+                    "inlineToolsClass" : ".js-inline-tool",
+                    "allPluginsButtonClass" : ".js-all-plugins-button"
+                }
+            ]
         </module-settings>
 
         <div id="codex-editor"></div>
@@ -75,6 +84,67 @@
             </a>
         </div>
     </div>
+    <?
+        $plugins = array(
+            array(
+                'name' => 'Header',
+                'type' => 'Block',
+                'js-class' => 'js-block-tool',
+                'description' => 'How will you live without headers?',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'n0str', 'photo' => 'https://avatars1.githubusercontent.com/u/988885?v=4&s=60' )
+                ),
+                'url' => 'github.com/codex-editor/header'
+            ),
+            array(
+                'name' => 'Marker',
+                'type' => 'Inline Tool',
+                'js-class' => 'js-inline-tool',
+                'description' => 'Highlight text fragments in your beautiful articles.',
+                'contributors' => array(
+                    array( 'name' => 'PolinaShneider', 'photo' => 'https://avatars3.githubusercontent.com/u/15448200?s=460&v=4' )
+                ),
+                'url' => 'github.com/codex-editor/marker'
+            ),
+            array(
+                'name' => 'Personality',
+                'type' => 'Block',
+                'js-class' => 'js-block-tool',
+                'description' => 'Beautiful widgets for beautiful persons.',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' )
+                ),
+                'url' => 'github.com/codex-editor/personality'
+            ),
+            array(
+                'name' => 'Code',
+                'type' => 'Block',
+                'js-class' => 'js-block-tool',
+                'description' => 'Include code examples in your writings.',
+                'contributors' => array(
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'PolinaShneider', 'photo' => 'https://avatars3.githubusercontent.com/u/15448200?s=460&v=4' )
+                ),
+                'url' => 'github.com/codex-editor/code'
+            ),
+            array(
+                'name' => 'Link',
+                'type' => 'Inline Tool',
+                'js-class' => 'js-inline-tool',
+                'description' => 'Embed links in your articles.',
+                'contributors' => array(
+                    array( 'name' => 'neSpecc', 'photo' => 'https://avatars0.githubusercontent.com/u/3684889?v=4&s=60' ),
+                    array( 'name' => 'talyguryn', 'photo' => 'https://avatars1.githubusercontent.com/u/15259299?v=4&s=60' ),
+                    array( 'name' => 'khaydarov', 'photo' => 'https://avatars1.githubusercontent.com/u/6507765?s=400&v=4' ),
+                ),
+                'url' => 'github.com/codex-editor/link'
+            )
+
+        )
+    ?>
     <div class="editor-landing__plugins">
         <h2 class="editor-landing__plugins-title">
             Best plugins
@@ -83,38 +153,41 @@
             Plugins can represent any Blocks: Quotes, Galleries, Polls, Embeds, Tables — anything you need. Also they can implement Inline Tools such as Marker, Term, Comments etc.
         </p>
         <div class="editor-landing__plugins-filter">
-            <a class="editor-landing__plugins-filter-button" href="#">
-                <? include(DOCROOT . '/public/app/landings/editor/svg/blocks-icon.svg'); ?>
+            <span class="editor-landing__plugins-filter-button js-block-tools-filter">
+                <? include(DOCROOT . '/public/app/landings/editor/svg/plus-icon.svg'); ?>
                 Blocks
-            </a>
-            <a class="editor-landing__plugins-filter-button" href="#">
-                <? include(DOCROOT . '/public/app/landings/editor/svg/inline-icon.svg'); ?>
+            </span>
+            <span class="editor-landing__plugins-filter-button js-inline-tools-filter">
+                <? include(DOCROOT . '/public/app/landings/editor/svg/marker-icon.svg'); ?>
                 Inline Tools
-            </a>
+            </span>
         </div>
-        <div class="editor-plugin clearfix">
-            <a href="#">
-                <h3 class="editor-plugin__title">Twitter</h3>
-                <span class="editor-plugin__label">Block</span>
-            </a>
-            <div class="editor-plugin__gif"></div>
-            <div class="editor-plugin__description">Include Tweets in your beautiful articles</div>
-            <div class="editor-plugin__contributors">
-                <a href="#" class="editor-plugin__contributors-item"></a>
-                <a href="#" class="editor-plugin__contributors-item"></a>
+        <? foreach ( $plugins as $plugin ): ?>
+            <div class="editor-plugin clearfix <?= $plugin['js-class'] ?>">
+                <a href="//<?= $plugin['url'] ?>" target="_blank">
+                    <h3 class="editor-plugin__title"><?= $plugin['name'] ?></h3>
+                    <span class="editor-plugin__label"><?= $plugin['type'] ?></span>
+                </a>
+                <div class="editor-plugin__gif"></div>
+                <div class="editor-plugin__description"><?= $plugin['description'] ?></div>
+                <div class="editor-plugin__contributors">
+                    <? foreach ( $plugin['contributors'] as $contributor ): ?>
+                        <a href="//github.com/<?= $contributor['name']; ?>" class="editor-plugin__contributors-item" title="<?= $contributor['name'] ?>" target="_blank">
+                            <img src="<?= $contributor['photo'] ?>" alt="<?= $contributor['name'] ?>">
+                        </a>
+                    <? endforeach; ?>
+                </div>
             </div>
-        </div>
-        <div class="editor-plugin clearfix">
-            <a href="#">
-                <h3 class="editor-plugin__title">Header</h3>
-                <span class="editor-plugin__label">Block</span>
+        <? endforeach; ?>
+        <div class="editor-landing__actions clearfix">
+            <span class="editor-landing__more-plugins js-all-plugins-button">
+                <? include(DOCROOT . '/public/app/landings/editor/svg/arrow-icon.svg'); ?>
+                View all plugins
+            </span>
+            <a class="editor-landing__contribute" href="#">
+                <? include(DOCROOT . '/public/app/landings/editor/svg/plus-icon.svg'); ?>
+                Contribute your plugin to this featured list
             </a>
-            <div class="editor-plugin__gif"></div>
-            <div class="editor-plugin__description">How will you live without headers?</div>
-            <div class="editor-plugin__contributors">
-                <a href="#" class="editor-plugin__contributors-item"></a>
-                <a href="#" class="editor-plugin__contributors-item"></a>
-            </div>
         </div>
     </div>
 </div>
---
+++ www/public/app/js/modules/pluginsFilter.js
@@ -0,0 +1,158 @@
+'use strict';
+
+/**
+ * Plugins filter on editor landing page
+ */
+module.exports = (function () {
+
+    /**
+     * Block plugins
+     */
+    let blockTools;
+
+    /**
+     * Inline Tool plugins
+     */
+    let inlineTools;
+
+    /**
+     * Button to show only Block plugins
+     */
+    let blockFilterButton;
+
+    /**
+     * Button to show only Inline Tool plugins
+     */
+    let inlineFilterButton;
+
+    /**
+     * Button to show all types of plugins
+     */
+    let allPluginsButton;
+
+    /**
+     * Initialize module
+     * @param {Object} settings                         - module's parameters passed from ModuleDispatcher
+     * @param {String} settings.blockToolsClass         - class of Editor Block Tools
+     * @param {String} settings.inlineToolsClass        - class of Editor Inline Tools
+     * @param {String} settings.blockFilterButtonClass  - class of button Block Tools filter
+     * @param {String} settings.inlineFilterButtonClass - class of button Inline Tools filter
+     * @param {String} settings.allPluginsButtonClass   - class of button showing all types of Tools
+     */
+    const init = function (settings) {
+
+        blockTools = document.querySelectorAll(settings.blockToolsClass);
+        inlineTools = document.querySelectorAll(settings.inlineToolsClass);
+        blockFilterButton = document.querySelector(settings.blockFilterButtonClass);
+        inlineFilterButton = document.querySelector(settings.inlineFilterButtonClass);
+        allPluginsButton = document.querySelector(settings.allPluginsButtonClass);
+
+        /**
+         * Add event listeners if filter buttons exist, otherwise show console messages
+         */
+        if (blockFilterButton) {
+
+            blockFilterButton.addEventListener('click', function () {
+
+                showBlockToolsOnly();
+
+            });
+
+        } else {
+
+            console.warn('Can\'t find button with class: «' + settings.blockFilterButtonClass + '»');
+
+        }
+
+        if (inlineFilterButton) {
+
+            inlineFilterButton.addEventListener('click', function () {
+
+                showInlineToolsOnly();
+
+            });
+
+        } else {
+
+            console.warn('Can\'t find output target with class: «' + settings.inlineFilterButtonClass + '»');
+
+        }
+
+        if (allPluginsButton) {
+
+            allPluginsButton.addEventListener('click', function () {
+
+                showAllPlugins();
+
+            });
+
+        } else {
+
+            console.warn('Can\'t find output target with class: «' + settings.allPluginsButtonClass + '»');
+
+        }
+
+    };
+
+    /**
+     * Show only Blocks, hide Inline Tools
+     */
+    const showBlockToolsOnly = function () {
+
+        for (let i = 0; i < inlineTools.length; i ++) {
+
+            inlineTools[i].classList.add('hide');
+
+        }
+
+        for (let i = 0; i < blockTools.length; i ++) {
+
+            blockTools[i].classList.remove('hide');
+
+        }
+
+    };
+
+    /**
+     * Show only Inline Tools, hide Blocks
+     */
+    const showInlineToolsOnly = function () {
+
+        for (let i = 0; i < blockTools.length; i ++) {
+
+            blockTools[i].classList.add('hide');
+
+        }
+
+        for (let i = 0; i < inlineTools.length; i ++) {
+
+            inlineTools[i].classList.remove('hide');
+
+        }
+
+    };
+
+    /**
+     * Show all types of Editor Tools
+     */
+    const showAllPlugins = function () {
+
+        for (let i = 0; i < blockTools.length; i ++) {
+
+            blockTools[i].classList.remove('hide');
+
+        }
+
+        for (let i = 0; i < inlineTools.length; i ++) {
+
+            inlineTools[i].classList.remove('hide');
+
+        }
+
+    };
+
+    return {
+        init : init
+    };
+
+}({}));
