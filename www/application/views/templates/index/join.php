e761f22a420564f8e5d3194e091cb49edacc3d38
Polina Shneider <polina@MacBook-Air.local>
2018-09-18 19:17:57 +0300

    Join component on main page

---
+++ www/application/views/templates/index/join.php
@@ -0,0 +1,17 @@
+<section class="join-component site-section">
+    <h2 class="site-section__title">Join Team</h2>
+    <div class="join-component__desc site-section__desc">
+        We are opening the new development season and invite developers, designers, QA engineers and PR eagles.<br><br>
+        Joining opened till 15th, october 2018
+    </div>
+
+    <time class="join-component__time">
+        <span data-time="days" class="join-component__time-item">14</span>
+        <span class="join-component__time-delimiter">:</span>
+        <span data-time="hrs" class="join-component__time-item">20</span>
+        <span class="join-component__time-delimiter">:</span>
+        <span data-time="mins" class="join-component__time-item">56</span>
+    </time>
+
+    <a href="/join" class="join-component__button buttons">Join</a>
+</section>
