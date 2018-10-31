86007f252d200e8fd2c655cad1d6e27436fe736e
Polina Shneider <beatrixlinkoln@gmail.com>
2018-10-31 17:41:57 +0300

    Change font-weight again

--- www/application/classes/Controller/Cover/Image.php
+++ www/application/classes/Controller/Cover/Image.php
@@ -121,7 +121,7 @@ class Controller_Cover_Image extends Controller_Base_preDispatch
 
         $font = new \SocialCoversGenerator\Properties\Font();
         $font->setColor($font_color);
-        $font->setFile(sprintf('%s/public/fonts/Roboto/%s', DOCROOT, 'Roboto-Medium.ttf'));
+        $font->setFile(sprintf('%s/public/fonts/Roboto/%s', DOCROOT, 'Roboto-Bold.ttf'));
         $font->setSize(48);
 
         $title->setFont($font);
