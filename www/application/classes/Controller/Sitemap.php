aefd061b3a84dbd104a014f61a341a03d7d05719
PolinaShneider <beatrixlinkoln@gmail.com>
2019-02-05 15:23:40 +0300

    Improve XML structure

--- www/application/classes/Controller/Sitemap.php
+++ www/application/classes/Controller/Sitemap.php
@@ -73,7 +73,10 @@ class Controller_Sitemap extends Controller_Base_preDispatch
 
         $sitemap = $this->fill_sitemap_data();
 
-        $xml = new SimpleXMLElement('<xml/>');
+        $xml = new SimpleXMLElement('<urlset/>');
+        $xml->addAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+        $xml->addAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $xml->addAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
 
         foreach ($sitemap->items as $item) {
             $track = $xml->addChild('url');
