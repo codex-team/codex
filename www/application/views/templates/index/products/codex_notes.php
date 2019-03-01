3fcfc526a792714fc6d3a27f56d39ea0d61f63ae
Taly <vitalik7tv@yandex.ru>
2019-03-01 11:59:22 +0300

    Update link for CodeX Notes 2

--- www/application/views/templates/index/products/codex_notes.php
+++ www/application/views/templates/index/products/codex_notes.php
@@ -3,19 +3,19 @@
         CodeX Notes
     </a>
     <div class="product-card__caption">
-        Simple, fast and useful crossplatform notes application based on CodeX Editor and Electron
+        Simple, fast and useful crossplatform notes application based on Editor.js and Electron
         <div class="product-card__actions">
-            <a class="product-card__download product-card__download--mac" href="https://github.com/codex-team/codex.notes/releases/download/v1.0/codex.notes_1.0.0_darwin.dmg">
+            <a class="product-card__download product-card__download--mac" href="https://github.com/codex-team/codex.notes/releases/download/v2.2.3/codex.notes-2.2.3.dmg">
                 <? include(DOCROOT . 'public/app/img/products/apple.svg'); ?>
                 Download
                 <b>for macOS</b>
             </a>
-            <a class="product-card__download product-card__download--win" href="https://github.com/codex-team/codex.notes/releases/download/v1.0/codex.notes_1.0.0_win32-x64.zip">
+            <a class="product-card__download product-card__download--win" href="https://github.com/codex-team/codex.notes/releases/download/v2.2.3/codex.notes-setup-2.2.3.exe">
                 <? include(DOCROOT . 'public/app/img/products/windows.svg'); ?>
                 Download
                 <b>for Windows</b>
             </a>
-            <a class="product-card__download product-card__download--linux" href="https://github.com/codex-team/codex.notes/releases/download/v1.0/codex.notes_1.0.0_amd64.deb">
+            <a class="product-card__download product-card__download--linux" href="https://github.com/codex-team/codex.notes/releases/download/v2.2.3/codex.notes-2.2.3-x86_64.AppImage">
                 <? include(DOCROOT . 'public/app/img/products/linux.svg'); ?>
                 Download
                 <b>for Linux</b>
