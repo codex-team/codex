8a347a8e89f45142eabe943517f56c616c2c3038
Taly <vitalik7tv@yandex.ru>
2019-04-03 00:27:38 +0300

    Merge branch 'master' into lazy-load

--- www/public/app/js/modules/reactions.js
+++ www/public/app/js/modules/reactions.js
@@ -1,55 +1,57 @@
-const Reactions = require('@codexteam/reactions');
+module.exports = require('@codexteam/reactions');
 
-/**
- * Module for pages using Editor
- */
-class ReactionsModule {
-
-    /**
-     * Create a new Reactions instance
-     * @param {object} settings — module settings
-     * @param {string} settings.holderId — name for a Reactions holder element
-     */
-    init(settings) {
-
-        /**
-         * If holderId is missing then do nothing
-         */
-        if (!settings.holderId) return;
-
-        /**
-         * Try to find holder element with given id
-         * @type {HTMLElement}
-         */
-        const holder = document.getElementById(settings.holderId);
-
-        /**
-         * If holder element is missing then do nothing
-         */
-        if (!holder) return;
-
-        /**
-         * Init Reactions module
-         */
-        return new Reactions({
-            /**
-             * Holder element
-             */
-            parent: holder,
-
-            /**
-             * Text before buttons
-             */
-            title: '',
-
-            /**
-             * Define buttons
-             */
-            reactions: ['👍', '❤️', '👎']
-        });
-
-    }
-
-}
-
-module.exports = new ReactionsModule();
+// const Reactions = require('@codexteam/reactions');
+//
+// /**
+//  * Module for pages using Editor
+//  */
+// class ReactionsModule {
+//
+//     /**
+//      * Create a new Reactions instance
+//      * @param {object} settings — module settings
+//      * @param {string} settings.holderId — name for a Reactions holder element
+//      */
+//     init(settings) {
+//
+//         /**
+//          * If holderId is missing then do nothing
+//          */
+//         if (!settings.holderId) return;
+//
+//         /**
+//          * Try to find holder element with given id
+//          * @type {HTMLElement}
+//          */
+//         const holder = document.getElementById(settings.holderId);
+//
+//         /**
+//          * If holder element is missing then do nothing
+//          */
+//         if (!holder) return;
+//
+//         /**
+//          * Init Reactions module
+//          */
+//         return new Reactions({
+//             /**
+//              * Holder element
+//              */
+//             parent: `#${settings.holderId}`,
+//
+//             /**
+//              * Text before buttons
+//              */
+//             title: '',
+//
+//             /**
+//              * Define buttons
+//              */
+//             reactions: ['👍', '❤️', '👎']
+//         });
+//
+//     }
+//
+// }
+//
+// module.exports = new ReactionsModule();
