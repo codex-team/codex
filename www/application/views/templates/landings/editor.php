ca0793e66b958fccd645ae97572c17c10e6034cc
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-04-05 09:32:22 +0530

    Grammatical Changes
    Grammatical Changes to API is the feature section on editor.js website.

--- www/application/views/templates/landings/editor.php
+++ www/application/views/templates/landings/editor.php
@@ -80,9 +80,9 @@
 
     <section class="editor-landing__section-header editor-landing__section-header--big">
         <header>
-            API is the feature.
+            The API is the feature.
         </header>
-        Each Block provided by plugin. It's really easy to create your own. Dozens of created.
+        Each Block provided by a plugin and it's really easy to create your own. Dozens have already been created.
     </section>
 
     <?
