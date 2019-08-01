4a9e0ba54a3eff07336d300a751fdf7683479a22
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-08-02 00:02:52 +0300

    Simplify getPrettifiedDtDisplay method with date() function

--- www/application/classes/Model/News.php
+++ www/application/classes/Model/News.php
@@ -132,23 +132,6 @@ class Model_News extends Model
      */
     public function getPrettifiedDtDisplay(): string
     {
-        static $numberToMonth = [
-            1 => 'jan',
-            2 => 'feb',
-            3 => 'mar',
-            4 => 'apr',
-            5 => 'may',
-            6 => 'jun',
-            7 => 'jul',
-            8 => 'aug',
-            9 => 'sep',
-            10 => 'oct',
-            11 => 'nov',
-            12 => 'dec'
-        ];
-
-        $parsedDate = date_parse($this->dt_display);
-
-        return sprintf('%s %s', $parsedDate['day'], $numberToMonth[$parsedDate['month']]);
+        return strtolower(date('j M', strtotime($this->dt_display)));
     }
 }
