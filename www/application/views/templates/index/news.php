154691c8e6aa3d02125fe03e4db5df29e1b18f82
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-08-17 17:35:12 +0300

    Revert to short php tag

--- www/application/views/templates/index/news.php
+++ www/application/views/templates/index/news.php
@@ -1,28 +1,28 @@
 <section class="site-section">
-    <?php
+    <?
         const MAX_PORTION = 3;
     ?>
-    <?php if (!empty($allNews)): ?>
+    <? if (!empty($allNews)): ?>
         <h2 class="site-section__title">Latest news</h2>
 
         <ul class="news js-emoji-included">
-            <?php $i = 0; ?>
-            <?php foreach ($allNews as $news ): ?>
+            <? $i = 0; ?>
+            <? foreach ($allNews as $news ): ?>
                 <li class="news__list_item <?= $i >= MAX_PORTION ? 'news__list_item--hidden' : ''?>" data-time="<?= $news->getPrettifiedDtDisplay() ?>">
-                    <?php if ($news->is_release): ?>
+                    <? if ($news->is_release): ?>
                         <span class="news__bage">✨release</span>
-                    <?php endif; ?>
-                    <?php if (isset($news->en_text)): ?>
+                    <? endif; ?>
+                    <? if (isset($news->en_text)): ?>
                         <?= $news->en_text ?>
-                    <?php else: ?>
+                    <? else: ?>
                         <?= $news->ru_text ?>
-                    <?php endif; ?>
+                    <? endif; ?>
                 </li>
-                <?php $i++; ?>
-            <?php endforeach; ?>
-            <?php if ($i > MAX_PORTION): ?>
+                <? $i++; ?>
+            <? endforeach; ?>
+            <? if ($i > MAX_PORTION): ?>
                 <span class="news__showmore" onclick="codex.showMoreNews.init( this );">Show more news</span>
-            <?php endif; ?>
+            <? endif; ?>
         </ul>
-    <?php endif; ?>
+    <? endif; ?>
 </section>
