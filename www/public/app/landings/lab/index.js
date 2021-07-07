e0bcd467be983a42f66c69e2940ce69c040a0f1e
Taly <vitalik7tv@yandex.ru>
2021-07-07 22:47:11 +0300

    commit changes

--- www/public/app/landings/lab/index.js
+++ www/public/app/landings/lab/index.js
@@ -1,1 +1,34 @@
+'use strict';
+
 require('./lab.pcss');
+
+module.exports = (() => {
+
+    console.log('hello');
+
+    const observer = new IntersectionObserver(entries => {
+
+
+        entries.forEach(entry => {
+
+            console.log('WOOW', entry);
+
+            // const square = entry.target.querySelector('.square');
+            //
+            // if (entry.isIntersecting) {
+            //
+            //     square.classList.add('square-animation');
+            //     return; // if we added the class, exit the function
+            //
+            // }
+            //
+            // // We're not intersecting, so remove the class!
+            // square.classList.remove('square-animation');
+
+        });
+
+    });
+
+    observer.observe(document.querySelector('.lab-page__cycle-list'));
+
+})();
