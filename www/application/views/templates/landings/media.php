7a67acec0f748c7729fe987cd94fd8b933403a03
Taly <vitalik7tv@yandex.ru>
2022-03-28 17:28:15 +0300

    Open pages in a new tab
    target="_blank"

--- www/application/views/templates/landings/media.php
+++ www/application/views/templates/landings/media.php
@@ -22,10 +22,10 @@
         </ul>
 
         <a class="codex-media__link" target="_blank" href="//school332.ru">Пример</a>
-        <a class="codex-media__link" href="//github.com/codex-team/codex.media">Исходный код</a>
+        <a class="codex-media__link" target="_blank" href="//github.com/codex-team/codex.media">Исходный код</a>
 
         <footer class="codex-media__footer clearfix">
-            <a class="codex-media__mail-link" href="//t.me/mediasite">Подключить свою школу</a>
+            <a class="codex-media__mail-link" target="_blank" href="//t.me/mediasite">Подключить свою школу</a>
             <!-- <a class="codex-media__mail-link" href="mailto:team@codex.so?subject=CodeX Media">Подключить свою школу</a> -->
             <div class="codex-media__footer-caption">St. Petersburg, Russia</div>
         </footer>
