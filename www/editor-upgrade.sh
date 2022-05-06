dd3a5a2b0a6d9edef628208265128e804ec2e834
Taly <vitalik7tv@yandex.ru>
2022-05-06 19:00:37 +0300

    Add separate script file for upgrading editor (#657)
    * add separate script file
    
    * Build frontend
    
    * Update fetch-the-editor-upgrade.yml
    
    * Build frontend
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ www/editor-upgrade.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+yarn add -D \
+  @editorjs/editorjs@latest \
+  @editorjs/header@latest \
+  @editorjs/code@latest \
+  @editorjs/delimiter@latest \
+  @editorjs/list@latest \
+  @editorjs/link@latest \
+  @editorjs/quote@latest \
+  @editorjs/raw@latest \
+  @editorjs/image@latest \
+  @editorjs/table@latest \
+  @editorjs/embed@latest \
+  @editorjs/inline-code@latest \
+  @editorjs/marker@latest
